1cc08c5abe5fbbb674078e7a06c4bb9cd11d5ec8
yu <yul1024@outlook.com>
2025-05-08 17:43:12 +0800

    Create vector_store_loader.py

---
+++ mas/graph/rag/vector_store_loader.py
@@ -0,0 +1,15 @@
+"""
+加载已经编码过的vector-store。
+"""
+
+
+class VectorStoreLoader:
+    def __init__(self):
+        ...
+
+    def get_vector_store(self, vector_store):
+        ...
+
+
+if __name__ == '__main__':
+    pass
