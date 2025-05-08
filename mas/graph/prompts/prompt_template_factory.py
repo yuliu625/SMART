55b350dc8d480937f4bb4833ea1afc5c2a22db5d
yu <yul1024@outlook.com>
2025-05-08 17:43:10 +0800

    Create prompt_template_factory.py

---
+++ mas/graph/prompts/prompt_template_factory.py
@@ -0,0 +1,16 @@
+"""
+封装通过文件加载prompt-template的过程。
+
+仅暴露可直接使用format方法的prompt-template对象。
+"""
+
+# from langchain.prompts import PromptTemplate
+
+
+class PromptTemplateFactory:
+    def __init__(self):
+        ...
+
+
+if __name__ == '__main__':
+    pass
