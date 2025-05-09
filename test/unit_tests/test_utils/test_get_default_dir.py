0a7afa6345d9e432a67e1566048761855cfb3e63
yu <yul1024@outlook.com>
2025-05-09 16:35:11 +0800

    Create test_get_default_dir.py

---
+++ test/unit_tests/test_utils/test_get_default_dir.py
@@ -0,0 +1,13 @@
+"""
+
+"""
+
+from mas.utils import get_default_dir
+
+
+def test_get_default_dir():
+    print(get_default_dir(base_dir=r"D:\dataset\risk_mas_t"))
+
+
+if __name__ == '__main__':
+    test_get_default_dir()
