9181e42098839cf628218cbf1c657fbe7c112dec
yu <yul1024@outlook.com>
2025-05-10 21:15:38 +0800

    Update prompt_template_factory.py

--- mas/graph/prompts/prompt_template_factory.py
+++ mas/graph/prompts/prompt_template_factory.py
@@ -4,13 +4,34 @@
 仅暴露可直接使用format方法的prompt-template对象。
 """
 
-# from langchain.prompts import PromptTemplate
+
+from mas.utils import load_prompt_template
+from pathlib import Path
+from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
+from langchain.prompts import PromptTemplate, MessagesPlaceholder, ChatPromptTemplate
+
+from langchain_core.messages import BaseMessage
+from langchain_core.prompts import BasePromptTemplate
 
 
 class PromptTemplateFactory:
-    def __init__(self):
-        ...
+    def __init__(
+        self,
+        prompts_dir: str | Path = None,
+    ):
+        if prompts_dir is None:
+            self.prompts_dir = Path(__file__).parent
+        else:
+            self.prompts_dir = Path(prompts_dir)
+
+    def get_system_prompt_template(
+        self,
+        agent_name: str,
+        **kwargs,
+    ) -> SystemMessage:
+        system_prompt_template = load_prompt_template(self.prompts_dir / f"{agent_name}_system_prompt_template.j2")
+        system_prompt = system_prompt_template.format(**kwargs)
+        return SystemMessage(system_prompt)
 
 
-if __name__ == '__main__':
-    pass
+
