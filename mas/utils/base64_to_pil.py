da22334c31c9d8951f3eafd21e3a6088731ff7b8
yu <yul1024@outlook.com>
2025-05-10 21:14:54 +0800

    Update base64_to_pil.py

--- mas/utils/base64_to_pil.py
+++ mas/utils/base64_to_pil.py
@@ -14,3 +14,10 @@ def base64_to_pil(
     image = Image.open(BytesIO(image_data))
     return image
 
+
+def uri_to_base64(
+    uri: str,
+) -> str:
+    with open(uri, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode("utf-8")
+
