ccd104f07cbe68c02a434da34d0fb58dc108a7f9
yu <yul1024@outlook.com>
2025-05-10 21:15:04 +0800

    Update llm_factory.py

--- mas/models/llm_factory.py
+++ mas/models/llm_factory.py
@@ -2,6 +2,7 @@
 生成LLM的工厂。
 """
 
+from langchain_openai import ChatOpenAI
 
 import os
 
@@ -12,18 +13,16 @@ class LLMFactory:
     def __init__(self):
         ...
 
-    # def get_qwen_llm(
-    #     self,
-    #     model: Annotated[str, "qwen系列模型的名字"] = 'qwen-max'
-    # ) -> OpenAILike:
-    #     llm = OpenAILike(
-    #         model=model,
-    #         api_base=os.environ['DASHSCOPE_API_BASE_URL'],
-    #         api_key=os.environ['DASHSCOPE_API_KEY'],
-    #         is_chat_model=True,
-    #         is_function_calling_model=True,
-    #     )
-    #     return llm
+    def get_qwen_llm(
+        self,
+        model: Annotated[str, "qwen系列模型的名字"] = 'qwen-max'
+    ) -> ChatOpenAI:
+        llm = ChatOpenAI(
+            model=model,
+            base_url=os.environ['DASHSCOPE_API_BASE_URL'],
+            api_key=os.environ['DASHSCOPE_API_KEY'],
+        )
+        return llm
 
 
 if __name__ == '__main__':
