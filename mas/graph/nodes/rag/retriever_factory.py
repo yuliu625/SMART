4f79dedc9cec0902852b5688611dc0f1e6ecc8d4
yu <yul1024@outlook.com>
2025-05-12 10:28:01 +0800

    Create retriever_factory.py

---
+++ mas/graph/nodes/rag/retriever_factory.py
@@ -0,0 +1,17 @@
+"""
+
+"""
+
+from mas.utils import VectorStoreLoader
+
+
+class RagRetrieverFactory:
+    def __init__(
+        self,
+    ):
+        ...
+
+    def get_simple_retriever(self):
+        ...
+
+
