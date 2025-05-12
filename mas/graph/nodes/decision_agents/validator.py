e6d513fe7f01a24e96b14e105354eea22eeac249
yu <yul1024@outlook.com>
2025-05-12 10:28:07 +0800

    Update validator.py

--- mas/graph/nodes/decision_agents/validator.py
+++ mas/graph/nodes/decision_agents/validator.py
@@ -2,14 +2,19 @@
 识别者。
 """
 
+from ...states import MASState
+
+from langchain_core.runnables import Runnable
+
 
 class Validator:
     def __init__(
         self,
+        llm_chain: Runnable,
     ):
-        ...
+        self.llm_chain = llm_chain
 
-    def run(self, state):
+    def run(self, state: MASState):
         ...
 
 
