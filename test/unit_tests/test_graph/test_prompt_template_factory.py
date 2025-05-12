a2158a0a9bcbc4d453304a9b095cde56156e6c30
yu <yul1024@outlook.com>
2025-05-12 10:26:43 +0800

    Update test_prompt_template_factory.py

--- test/unit_tests/test_graph/test_prompt_template_factory.py
+++ test/unit_tests/test_graph/test_prompt_template_factory.py
@@ -5,10 +5,15 @@
 from mas.graph.prompts import PromptTemplateFactory
 
 
-def test_prompt_template_factory():
-    prompt_template_factory = PromptTemplateFactory()
+def test_prompt_template_factory_1():
+    prompt_template_factory = PromptTemplateFactory('all')
+    print(prompt_template_factory.get_system_prompt_template('document_reader'))
+
+
+def test_prompt_template_factory_2():
+    prompt_template_factory = PromptTemplateFactory('all')
     print(prompt_template_factory.get_system_prompt_template('recognizer'))
 
 
 if __name__ == '__main__':
-    test_prompt_template_factory()
+    test_prompt_template_factory_1()
