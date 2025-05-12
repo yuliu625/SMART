5076e70ef65bb12af3c1f30d1f59a6f9b99e78b0
yu <yul1024@outlook.com>
2025-05-12 10:26:37 +0800

    Update test_load_prompt_template.py

--- test/unit_tests/test_utils/test_load_prompt_template.py
+++ test/unit_tests/test_utils/test_load_prompt_template.py
@@ -7,7 +7,7 @@ from mas.utils import load_prompt_template
 
 def test_load_prompt_template():
     prompt_template = load_prompt_template(
-        '../../../mas/graph/prompts/recognizer_system_prompt_template.j2'
+        '../../../mas/graph/prompts/all/recognizer_system_prompt_template.j2'
     )
     print(prompt_template)
     print(prompt_template.format())
