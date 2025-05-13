abb4a143b7160c589059f5962372aa480543b61d
yu <yul1024@outlook.com>
2025-05-13 15:40:42 +0800

    Update agent_factory.py

--- mas/graph/nodes/agent_factory.py
+++ mas/graph/nodes/agent_factory.py
@@ -22,26 +22,33 @@ class AgentFactory:
         self.prompt_template_factory = PromptTemplateFactory()
         self.llm_factory = LLMFactory()
 
+    def get_rag_agent(self):
+        rag_system_message = self.prompt_template_factory.get_system_message(agent_name='rag')
+        rag_with_system_prompt_template = self._get_with_system_prompt_template(
+            system_message=rag_system_message,
+        )
+        return rag_with_system_prompt_template
+
     def get_recognizer(self):
-        recognizer_system_prompt = self.prompt_template_factory.get_system_prompt_template(agent_name='recognizer')
+        recognizer_system_message = self.prompt_template_factory.get_system_message(agent_name='recognizer')
         recognizer_with_system_prompt_template = self._get_with_system_prompt_template(
-            system_message=recognizer_system_prompt
+            system_message=recognizer_system_message
         )
         recognizer = Recognizer(llm_chain=recognizer_with_system_prompt_template)
         return recognizer
 
     def get_validator(self):
-        validator_system_prompt = self.prompt_template_factory.get_system_prompt_template(agent_name='validator')
+        validator_system_message = self.prompt_template_factory.get_system_message(agent_name='validator')
         validator_with_system_prompt_template = self._get_with_system_prompt_template(
-            system_message=validator_system_prompt
+            system_message=validator_system_message
         )
         validator = Validator(llm_chain=validator_with_system_prompt_template)
         return validator
 
     def get_arbiter(self):
-        arbiter_system_prompt = self.prompt_template_factory.get_system_prompt_template(agent_name='arbiter')
+        arbiter_system_message = self.prompt_template_factory.get_system_message(agent_name='arbiter')
         arbiter_with_system_prompt_template = self._get_with_system_prompt_template(
-            system_message=arbiter_system_prompt
+            system_message=arbiter_system_message
         )
         arbiter = Arbiter(llm_chain=arbiter_with_system_prompt_template)
         return arbiter
