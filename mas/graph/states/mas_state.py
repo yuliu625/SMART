a9ef7bda4598141c781fcf5727485f64c2d70a78
yu <yul1024@outlook.com>
2025-05-13 15:40:16 +0800

    Update mas_state.py

--- mas/graph/states/mas_state.py
+++ mas/graph/states/mas_state.py
@@ -6,18 +6,59 @@ from langchain_core.messages import AnyMessage
 from typing import TypedDict, Any
 from pydantic import BaseModel, Field
 
+from langchain_core.documents import Document
+
 
 class MASState(BaseModel):
-    messages: list[AnyMessage] = []
+    # rag
+    query_chat_history: list[Any] = Field(
+        default_factory=list,
+        description="查询的历史，用于rag中的agent进行对话。"
+    )
+    # # result history
+    query_result_history: list[Document] = Field(
+        default_factory=list,
+        description="历史查询的结果，返回新查询结果是用于去重。",
+    )
+    current_query: str = Field(
+        description="当前的查询。"
+    )
+    last_round_result_number: int = Field(
+        default=-1,
+        description="上轮获取的结果的数量"
+    )
+    # # query result
+    current_query_results: list[Document] = Field(
+        default_factory=list,
+        description="当前轮返回的最终结果。"
+    )
     # analysis agents
 
     # decision agents
-    recognizer_history: list[AnyMessage] = []
-    validator_history: list[AnyMessage] = []
-    arbiter_decision: list[AnyMessage] = []
+    recognizer_history: list[Any] = Field(
+        default_factory=list,
+        description="",
+    )
+    validator_history: list[Any] = Field(
+        default_factory=list,
+        description="",
+    )
+    arbiter_decision: list[Any] = Field(
+        default_factory=list,
+        description="",
+    )
     # validator related
     # # 剩余验证步骤，如果用完，就必须交给arbiter进行最终仲裁。
-    remaining_validation_rounds: int = 3
+    remaining_validation_rounds: int = Field(
+        default=3,
+        description="",
+    )
     # # validator要求analysis模块要进行重新分析的{agent: content}。
-    is_need_verification: bool = False
-    verification_requests: dict[str, str]
+    is_need_verification: bool = Field(
+        default=False,
+        description="",
+    )
+    verification_requests: dict[str, str] = Field(
+        default_factory=dict,
+        description=""
+    )
