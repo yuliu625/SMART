2e9514d8c35ad59d0c4d2cc7eacdc1eb148b093d
yu <yul1024@outlook.com>
2025-05-13 15:41:15 +0800

    Update build_document_store.py

--- mas/data_processing/loading/build_document_store.py
+++ mas/data_processing/loading/build_document_store.py
@@ -19,10 +19,11 @@ def build_document_store(
         sub_dir_dict = get_default_dir(base_dir)
 
     batch_processor = BatchProcessor(
-        original_pdf_dir=sub_dir_dict['original_pdf_dir'],
-        base_image_pdf_dir=sub_dir_dict['base_image_pdf_dir'],
-        text_document_store_dir=sub_dir_dict['text_document_store_dir'],
-        image_document_store_dir=sub_dir_dict['image_document_store_dir'],
+        base_dir=base_dir,
+        original_pdf_dir=sub_dir_dict.original_pdf_dir,
+        base_image_pdf_dir=sub_dir_dict.base_image_pdf_dir,
+        text_document_store_dir=sub_dir_dict.text_document_store_dir,
+        image_document_store_dir=sub_dir_dict.image_document_store_dir,
     )
     batch_processor.run(
         text_loading_methods=text_loading_methods,
