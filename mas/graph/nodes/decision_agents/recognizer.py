fddc720c96eebdaa09d10f9e829d2dbcaeddd331
yu <yul1024@outlook.com>
2025-05-15 19:44:30 +0800

    Update recognizer.py

--- mas/graph/nodes/decision_agents/recognizer.py
+++ mas/graph/nodes/decision_agents/recognizer.py
@@ -5,13 +5,16 @@
 from ...states import MASState
 from ..base_agent import BaseAgent
 
+from langchain_core.messages import HumanMessage
+
 from langchain_core.runnables import Runnable
-from pydantic import BaseModel
 
 
 class Recognizer(BaseAgent):
     """
+    整体公司风险识别者。
 
+    长文档阅读能力。
     """
     def __init__(
         self,
@@ -22,5 +25,15 @@ class Recognizer(BaseAgent):
         )
 
     def run(self, state: MASState):
-        ...
+        response = self.call_llm_chain(chat_history=[HumanMessage(state.original_pdf_text)])
+        validator_chat_history = [HumanMessage(content=response.content)]
+        arbiter_contex = [(
+            "<!--recognizer-start-->"
+            + response.content
+            + "<!--recognizer-end-->"
+        )]
+        return {
+            'validator_chat_history': validator_chat_history,
+            'arbiter_contex': arbiter_contex,
+        }
 
