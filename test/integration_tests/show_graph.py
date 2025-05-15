e894e46d124f2877496a8b43676052491b4b4ef3
yu <yul1024@outlook.com>
2025-05-15 10:10:02 +0800

    Update show_graph.py

--- test/integration_tests/show_graph.py
+++ test/integration_tests/show_graph.py
@@ -3,11 +3,11 @@
 """
 
 from mas.utils import save_graph_png
-from mas.graph import MASGraph
+from mas.graph import MASGraphBuilder
 
 
 def show_graph():
-    mas_graph = MASGraph()
+    mas_graph = MASGraphBuilder()
     graph = mas_graph.build_graph()
     save_graph_png(graph, output_path='./t.png')
 
