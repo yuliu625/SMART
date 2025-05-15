cb428f5eb4303cf6e0f1c693ea6eab9c24520270
yu <yul1024@outlook.com>
2025-05-15 10:11:43 +0800

    Update decision_edges.py

--- mas/graph/edges/decision_edges.py
+++ mas/graph/edges/decision_edges.py
@@ -2,15 +2,14 @@
 
 """
 
-from langgraph.graph.graph import (
-    END,
-    START,
-)
+from ..states import MASState
+
+from langgraph.graph.graph import END
 
 from typing import Literal
 
 
-def condition_final_round(state) -> Literal[END, 'recognizer']:
+def condition_final_round(state: MASState) -> Literal[END, 'recognizer']:
     if state.remaining_validation_rounds == 0:
         return END
     else:
