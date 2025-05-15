99cb16f722d55fda06b50055187b962d41b346e4
yu <yul1024@outlook.com>
2025-05-15 10:10:54 +0800

    Update validator.py

--- mas/graph/nodes/decision_agents/validator.py
+++ mas/graph/nodes/decision_agents/validator.py
@@ -3,15 +3,26 @@
 """
 
 from ...states import MASState
+from ..base_agent import BaseAgent
 
 from langchain_core.runnables import Runnable
+from pydantic import BaseModel
 
 
-class Validator:
+class Validator(BaseAgent):
+    """
+
+    """
     def __init__(
         self,
         llm_chain: Runnable,
+        structured_output_format: type[BaseModel],
     ):
+        super().__init__(
+            llm_chain=llm_chain,
+            is_need_structured_output=True,
+            structured_output_format=structured_output_format,
+        )
         self.llm_chain = llm_chain
 
     def run(self, state: MASState):
