d9203ebb004cb59b93517a6a119369a462c2b001
yu <yul1024@outlook.com>
2025-05-15 10:10:15 +0800

    Update structured_output_format.py

--- mas/graph/states/structured_output_format.py
+++ mas/graph/states/structured_output_format.py
@@ -5,7 +5,12 @@
 from pydantic import BaseModel, Field
 
 
-class Confidence(BaseModel):
+class RequestAgent(BaseModel):
+    agent_name: str = Field(description="某个agent对于下一个请求的agent的名字。")
+    message: str = Field(description="某个agent对于下一个请求的agent的内容。")
+
+
+class ConfidenceOutput(BaseModel):
     content: str
     confidence: float
 
