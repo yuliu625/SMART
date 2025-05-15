bf91c35a039e72d6e64bda0c9ebaea4fdef448b5
yu <yul1024@outlook.com>
2025-05-15 10:10:11 +0800

    Create mas_runner.py

---
+++ mas/mas_runner.py
@@ -0,0 +1,16 @@
+"""
+
+"""
+
+from .graph import MASGraphBuilder
+
+
+class MASRunner:
+    def __init__(
+        self,
+        vectory_store,
+        pdf_name: str,
+    ):
+        self.mas_graph_builder = MASGraphBuilder(
+
+        )
