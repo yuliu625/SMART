0ef9e265a9d291b5423876476c79316b6c753a16
yu <yul1024@outlook.com>
2025-05-15 10:11:20 +0800

    Update agent_factory.py

--- mas/graph/nodes/agent_factory.py
+++ mas/graph/nodes/agent_factory.py
@@ -2,13 +2,21 @@
 
 """
 
+from .analysis_agents import (
+    BaseDocumentReader,
+    BaseAnalyst,
+)
 from .decision_agents import (
     Recognizer,
     Validator,
     Arbiter,
 )
+from ..states.structured_output_format import (
+    RequestAgent,
+)
 from mas.graph.prompts import PromptTemplateFactory
 from mas.models import LLMFactory
+
 
 from langchain_core.messages import AIMessage, HumanMessage, SystemMessage
 from langchain.prompts import PromptTemplate, MessagesPlaceholder, ChatPromptTemplate
@@ -16,42 +24,64 @@ from langchain.prompts import PromptTemplate, MessagesPlaceholder, ChatPromptTem
 
 class AgentFactory:
     """
+    生成graph中可以使用的node。
 
+    如果进行测试，使用策略模式封装的_get_llm_chain方法。
     """
     def __init__(self):
         self.prompt_template_factory = PromptTemplateFactory()
         self.llm_factory = LLMFactory()
 
-    def get_rag_agent(self):
-        rag_system_message = self.prompt_template_factory.get_system_message(agent_name='rag')
-        rag_with_system_prompt_template = self._get_with_system_prompt_template(
-            system_message=rag_system_message,
+    def get_document_reader(self):
+        document_llm_chain = self._get_llm_chain(llm_name='qwen-vl-max', agent_name='document_reader')
+        document_reader = BaseDocumentReader(
+            llm_chain=document_llm_chain,
         )
-        return rag_with_system_prompt_template
+        return document_reader
+
+    def get_analyst(self):
+        analyst_llm_chain = self._get_llm_chain(llm_name='qwen-max', agent_name='analyst')
+        analyst = BaseAnalyst(
+            llm_chain=analyst_llm_chain,
+            structured_output_format=RequestAgent,
+        )
+        return analyst
 
     def get_recognizer(self):
-        recognizer_system_message = self.prompt_template_factory.get_system_message(agent_name='recognizer')
-        recognizer_with_system_prompt_template = self._get_with_system_prompt_template(
-            system_message=recognizer_system_message
+        recognizer_llm_chain = self._get_llm_chain(llm_name='qwen-long', agent_name='recognizer')
+        recognizer = Recognizer(
+            llm_chain=recognizer_llm_chain,
         )
-        recognizer = Recognizer(llm_chain=recognizer_with_system_prompt_template)
         return recognizer
 
     def get_validator(self):
-        validator_system_message = self.prompt_template_factory.get_system_message(agent_name='validator')
-        validator_with_system_prompt_template = self._get_with_system_prompt_template(
-            system_message=validator_system_message
+        validator_llm_chain = self._get_llm_chain(llm_name='qwen-max', agent_name='validator')
+        validator = Validator(
+            llm_chain=validator_llm_chain,
+            structured_output_format=RequestAgent,
         )
-        validator = Validator(llm_chain=validator_with_system_prompt_template)
         return validator
 
     def get_arbiter(self):
-        arbiter_system_message = self.prompt_template_factory.get_system_message(agent_name='arbiter')
-        arbiter_with_system_prompt_template = self._get_with_system_prompt_template(
-            system_message=arbiter_system_message
+        arbiter_llm_chain = self._get_llm_chain(llm_name='qwen-max', agent_name='arbiter')
+        arbiter = Arbiter(
+            llm_chain=arbiter_llm_chain,
+            structured_output_format=None,
         )
-        arbiter = Arbiter(llm_chain=arbiter_with_system_prompt_template)
         return arbiter
+
+    def _get_llm_chain(
+        self,
+        llm_name: str,
+        agent_name: str,
+    ):
+        system_message = self.prompt_template_factory.get_system_message(agent_name=agent_name)
+        with_system_prompt_template = self._get_with_system_prompt_template(
+            system_message=system_message,
+        )
+        llm = self.llm_factory.get_qwen_llm(model=llm_name)
+        llm_chain = with_system_prompt_template | llm
+        return llm_chain
 
     def _get_with_system_prompt_template(
         self,
