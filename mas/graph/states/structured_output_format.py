deadd7e22f3aa69b00c2293753a48b44000dc88c
yu <yul1024@outlook.com>
2025-05-15 19:43:42 +0800

    Update structured_output_format.py

--- mas/graph/states/structured_output_format.py
+++ mas/graph/states/structured_output_format.py
@@ -10,7 +10,15 @@ class RequestAgent(BaseModel):
     message: str = Field(description="某个agent对于下一个请求的agent的内容。")
 
 
+class ArbiterDecision(BaseModel):
+    decision: str = Field(description="最终的决定。")
+    risk: bool = Field(description="是否存在风险的判断。")
+    confidence: float = Field(description="对于仲裁结果的置信度。")
+
+
 class ConfidenceOutput(BaseModel):
     content: str
     confidence: float
+
+
 
