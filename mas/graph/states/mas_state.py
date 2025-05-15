620f0d47918e41dfceaaa7941a3281156eba2d29
yu <yul1024@outlook.com>
2025-05-15 10:10:18 +0800

    Update mas_state.py

--- mas/graph/states/mas_state.py
+++ mas/graph/states/mas_state.py
@@ -13,7 +13,7 @@ class MASState(BaseModel):
     # rag
     query_chat_history: list[Any] = Field(
         default_factory=list,
-        description="查询的历史，用于rag中的agent进行对话。"
+        description="查询的历史，用于rag中的agent进行对话。",
     )
     # # result history
     query_result_history: list[Document] = Field(
@@ -21,29 +21,40 @@ class MASState(BaseModel):
         description="历史查询的结果，返回新查询结果是用于去重。",
     )
     current_query: str = Field(
-        description="当前的查询。"
+        description="当前的查询。",
     )
     last_round_result_number: int = Field(
         default=-1,
-        description="上轮获取的结果的数量"
+        description="上轮获取的结果的数量",
     )
     # # query result
     current_query_results: list[Document] = Field(
         default_factory=list,
-        description="当前轮返回的最终结果。"
+        description="当前轮返回的最终结果。",
     )
     # analysis agents
-
+    document_reader_history: list[AnyMessage] = Field(
+        default_factory=list,
+        description="文档阅读的历史",
+    )
+    more_information: bool = Field(
+        default=False,
+        description="analyst选择是否需要进一步的分析。",
+    )
+    analyst_chat_history: list[AnyMessage] = Field(
+        default_factory=list,
+        description="analyst过去获得的信息和进行的分析",
+    )
     # decision agents
-    recognizer_history: list[Any] = Field(
+    recognizer_history: list[AnyMessage] = Field(
         default_factory=list,
         description="",
     )
-    validator_history: list[Any] = Field(
+    validator_history: list[AnyMessage] = Field(
         default_factory=list,
         description="",
     )
-    arbiter_decision: list[Any] = Field(
+    arbiter_decision: list[AnyMessage] = Field(
         default_factory=list,
         description="",
     )
