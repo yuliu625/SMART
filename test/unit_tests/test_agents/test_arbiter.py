2142d0ce1b2cd01f55b2ea4d6c9ebd3ccce9e673
yu <yul1024@outlook.com>
2025-05-17 10:17:06 +0800

    Create test_arbiter.py

---
+++ test/unit_tests/test_agents/test_arbiter.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+
+from mas.graph.states import MASState
+from mas.graph.nodes import AgentFactory
+
+
+def test_arbiter_1():
+    agent_factory = AgentFactory()
+    arbiter = agent_factory.get_arbiter()
+
+
+if __name__ == '__main__':
+    test_arbiter_1()
