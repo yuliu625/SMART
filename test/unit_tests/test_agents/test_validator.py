8d6c3c2aa7d0020c7d28d9ff535f0c526407980a
yu <yul1024@outlook.com>
2025-05-17 10:16:55 +0800

    Create test_validator.py

---
+++ test/unit_tests/test_agents/test_validator.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+
+from mas.graph.states import MASState
+from mas.graph.nodes import AgentFactory
+
+
+def test_validator_1():
+    agent_factory = AgentFactory()
+    validator = agent_factory.get_validator()
+
+
+if __name__ == '__main__':
+    test_validator_1()
