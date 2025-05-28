2d3a722abf98d9bf36043b349579d0628296b3d4
yu <yul1024@outlook.com>
2025-05-28 16:20:15 +0800

    Update get_file_tree.py

--- mas/get_file_tree.py
+++ mas/get_file_tree.py
@@ -12,6 +12,22 @@
 使用:
     - 约定。所有文件都默认使用get_file_tree方法获取路径。
     - 项目迁移。仅修改_BASE_DIR。
+
+
+该项目文件树结构为:
+- base_dir
+    - original_pdf
+    - image_pdf
+    - document_store
+        - text
+            - ${loading_method}
+        - image
+            - ${loading_method}
+    - vector_store
+        - text
+            - ${embedding_method--loading_method}
+        - image
+            - ${embedding_method--loading_method}
 """
 
 from pathlib import Path
@@ -24,6 +40,15 @@ __BASE_DIR = r""
 class FileTree(BaseModel):
     """声明所有需要的路径。"""
     base_dir: Path = Field(description="最基础的路径，其他路径都由这个路径生成。")
+    original_pdf_dir: Path
+    txt_pdf_dir: Path
+    base_image_pdf_dir: Path
+    document_store_dir: Path
+    text_document_store_dir: Path
+    image_document_store_dir: Path
+    vector_store_dir: Path
+    text_vector_store_dir: Path
+    image_vector_store_dir: Path
 
 
 def get_file_tree(
@@ -42,6 +67,15 @@ def get_file_tree(
     # 这里写生成FileTree的方法
     file_tree = FileTree(
         base_dir=base_dir,
+        original_pdf_dir=base_dir / 'original_pdf',
+        txt_pdf_dir=base_dir / 'txt_pdf',
+        base_image_pdf_dir=base_dir / 'image_pdf',
+        document_store_dir=base_dir / 'document_store',
+        text_document_store_dir=base_dir / 'document_store' / 'text',
+        image_document_store_dir=base_dir / 'document_store' / 'image',
+        vector_store_dir=base_dir / 'vector_store',
+        text_vector_store_dir=base_dir / 'vector_store' / 'text',
+        image_vector_store_dir=base_dir / 'vector_store' / 'image',
     )
     return file_tree
 
