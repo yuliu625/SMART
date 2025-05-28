7abca0186858234f4153c49bd368495c06f1aa82
yu <yul1024@outlook.com>
2025-05-28 16:10:43 +0800

    Create prompt_template_factory.py

---
+++ mas/nodes/prompts/prompt_template_factory.py
@@ -0,0 +1,11 @@
+"""
+
+"""
+
+from .base_prompt_template_factory import BasePromptTemplateFactory
+
+
+class PromptTemplateFactory(BasePromptTemplateFactory):
+    def __init__(self):
+        ...
+
