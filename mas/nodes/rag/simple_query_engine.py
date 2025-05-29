b800584fecc9eebfdf66fd1fcb1ed7296e00c1bd
yu <yul1024@outlook.com>
2025-05-29 13:26:24 +0800

    Create simple_query_engine.py

---
+++ mas/nodes/rag/simple_query_engine.py
@@ -0,0 +1,25 @@
+"""
+
+"""
+
+from mas.schemas import MASState
+
+from langchain_core.vectorstores import VectorStoreRetriever
+
+
+class SimpleQueryEngine:
+    """
+
+    """
+    def __init__(
+        self,
+        retriever: VectorStoreRetriever,
+    ):
+        self.retriever = retriever
+
+    def run(self, state: MASState):
+        documents = self.retriever.invoke(state.current_message)
+        return {
+            f'{state.current_agent_name}_query_results': documents,
+        }
+
