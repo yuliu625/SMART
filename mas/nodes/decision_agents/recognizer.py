628fb2dcf67853e8fefd63661a77e167f014adf1
yu <yul1024@outlook.com>
2025-06-01 14:36:38 +0800

    Create recognizer.py

---
+++ mas/nodes/decision_agents/recognizer.py
@@ -0,0 +1,53 @@
+"""
+识别者。
+"""
+
+from __future__ import annotations
+
+from ..base_agent import BaseAgent
+
+from langchain_core.messages import HumanMessage
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from mas.schemas.decision_state import DecisionState
+    from langchain_core.runnables import Runnable
+    from langchain_core.language_models import BaseChatModel
+    from langchain_core.prompts import ChatPromptTemplate
+
+
+class Recognizer(BaseAgent):
+    """
+    整体公司风险识别者。
+
+    长文档阅读能力。
+    """
+    def __init__(
+        self,
+        chat_prompt_template: ChatPromptTemplate,
+        llm: BaseChatModel,
+    ):
+        super().__init__(
+            chat_prompt_template=chat_prompt_template,
+            llm=llm,
+            max_retries=10,
+            is_need_structured_output=False,
+            schema_pydantic_base_model=None,
+            schema_check_type='dict',
+        )
+
+    def process_state(self, state: DecisionState) -> dict:
+        # 对于初始pdf的文本内容进行分析。
+        response = self.call_llm_chain(chat_history=[HumanMessage(state.original_pdf_text)])
+        # 标注身份。
+        arbiter_context = [(
+                "<!--recognizer-start-->\n\n"
+                + response.content
+                + "\n\n<!--recognizer-end-->"
+        )]
+        validator_chat_history = [HumanMessage(content=arbiter_context)]
+        return {
+            'validator_chat_history': validator_chat_history,
+            'arbiter_contex': arbiter_context,
+        }
+
