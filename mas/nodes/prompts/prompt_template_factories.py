fc97be486626b405d232f9ade9e919a8dbf48902
yu <yul1024@outlook.com>
2025-06-12 10:48:15 +0800

    Update prompt_template_factories.py

---
+++ mas/nodes/prompts/prompt_template_factories.py
@@ -0,0 +1,15 @@
+"""
+
+"""
+
+from __future__ import annotations
+
+from .base_prompt_template_factory import BasePromptTemplateFactory
+
+
+class AllPromptTemplateFactory(BasePromptTemplateFactory):
+    def __init__(
+        self,
+    ):
+        ...
+
