ab1ef1a66eb82fe9ad3b059e90a8553457e124ae
yu <yul1024@outlook.com>
2025-06-12 16:58:15 +0800

    Update decision_state.py

--- mas/schemas/decision_state.py
+++ mas/schemas/decision_state.py
@@ -3,43 +3,52 @@
 """
 
 from __future__ import annotations
-
 from pydantic import BaseModel, Field
+
+from langchain_core.messages import AnyMessage
+
 from typing import TYPE_CHECKING
-if TYPE_CHECKING:
-    from langchain_core.messages import AnyMessage
+# if TYPE_CHECKING:
 
 
 class DecisionState(BaseModel):
-    # recognizer看到的初始文本内容。
+    # MAS共有
+    current_agent_name: str = Field(
+        description="",
+    )
+    current_message: str = Field(
+        description="",
+    )
+
+    # 初始。
+    # # recognizer看到的初始文本内容。
     original_pdf_text: str = Field(
         description="原始的pdf文档的text。专门用一个字段是为了避免和current_message出现问题。",
     )
+
+    # 控制。
     # # 剩余验证步骤，如果用完，就必须交给arbiter进行最终仲裁。
     remaining_validation_rounds: int = Field(
         default=5,
         description="validator剩余可进行验证的次数。",
     )
-    # decision agents
-    recognizer_chat_history: list[AnyMessage] = Field(
+
+    # agents相关。
+    # # decision agents
+    shared_chat_history: list[AnyMessage] = Field(
         default_factory=list,
-        description="recognizer通过长文本进行全面的判断",
+        description="共有的记忆。",
     )
-    validator_chat_history: list[AnyMessage] = Field(
-        default_factory=list,
-        description="validator进行各种验证的记录。",
-    )
-    arbiter_context: list[str] = Field(
-        default=list,
-        description="累计的validator的对话记录。"
-    )
-    arbiter_chat_history: list[AnyMessage] = Field(
-        default_factory=list,
-        description="arbiter进行最终的仲裁的分析内容。",
-    )
+    # recognizer_chat_history: list[AnyMessage] = Field(
+    #     default_factory=list,
+    #     description="recognizer通过长文本进行全面的判断",
+    # )
+    # validator_chat_history: list[AnyMessage] = Field(
+    #     default_factory=list,
+    #     description="validator进行各种验证的记录。",
+    # )
     arbiter_decision: dict = Field(
         default_factory=dict,
-        description="arbiter的结构化输出。"
+        description="arbiter的结构化输出。",
     )
-
 
