53b0f0e84fbdf65a6921c4b70e7027a6461adc8f
yu <yul1024@outlook.com>
2025-06-12 19:12:01 +0800

    Update prompt_template_factories.py

--- mas/nodes/prompts/prompt_template_factories.py
+++ mas/nodes/prompts/prompt_template_factories.py
@@ -1,15 +1,19 @@
 """
-
+为了对比实验，各种类型的prompt-template-factory。
 """
 
 from __future__ import annotations
 
 from .base_prompt_template_factory import BasePromptTemplateFactory
 
+# from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
 
 class AllPromptTemplateFactory(BasePromptTemplateFactory):
     def __init__(
         self,
     ):
-        ...
+        super().__init__()
+        self._set_sub_dir('all')
 
