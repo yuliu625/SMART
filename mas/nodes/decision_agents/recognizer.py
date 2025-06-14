9da3f4e7cc2c37e5d9378ee99aef582c2642f13f
yu <yul1024@outlook.com>
2025-06-14 20:33:02 +0800

    Update recognizer.py

--- mas/nodes/decision_agents/recognizer.py
+++ mas/nodes/decision_agents/recognizer.py
@@ -4,15 +4,18 @@
 
 from __future__ import annotations
 
-from ..base_agent import BaseAgent
+from mas.nodes.base_agent import BaseAgent
+from mas.schemas.structured_output_format import AgentProcessedResult
 
 from langchain_core.messages import HumanMessage
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from mas.schemas.decision_state import DecisionState
+    from langchain_core.runnables import RunnableConfig
     from langchain_core.language_models import BaseChatModel
     from langchain_core.prompts import ChatPromptTemplate
+    from langchain_core.messages import AnyMessage, AIMessage
 
 
 class Recognizer(BaseAgent):
@@ -35,23 +38,47 @@ class Recognizer(BaseAgent):
             schema_check_type='dict',
         )
 
-    def process_state(self, state: DecisionState) -> dict:
-        arbiter_context = self.recognize(original_pdf_text=state.original_pdf_text)
-        shared_chat_history = [HumanMessage(content=arbiter_context)]
+    def process_state(
+        self,
+        state: DecisionState,
+        config: RunnableConfig,
+    ) -> dict:
+        recognizer_result = self.recognize(original_pdf_text=state.original_pdf_text)
         return {
-            'shared_chat_history': shared_chat_history,
+            'shared_chat_history': recognizer_result.chat_history,
         }
 
     def recognize(
         self,
         original_pdf_text: str,
-    ) -> str:
+    ) -> AgentProcessedResult:
         # 对于初始pdf的文本内容进行分析。
         response = self.call_llm_with_retry(chat_history=[HumanMessage(content=original_pdf_text)])
-        # 标注身份。
-        arbiter_context = self.wrap_message_content_with_agent_name(
-            agent_name='recognizer',
-            original_content=response.content,
+        shared_chat_history = self.get_initial_shared_chat_history(recognizer_response=response)
+        return AgentProcessedResult(
+            chat_history=shared_chat_history,
+            agent_request=None,
         )
-        return arbiter_context
+
+    def get_initial_shared_chat_history(
+        self,
+        recognizer_response: AIMessage,
+    ) -> list[AnyMessage]:
+        """
+        将封装的方法
+
+        Args:
+            recognizer_response:
+
+        Returns:
+
+        """
+        # 标注身份。
+        arbiter_first_message_context = self.wrap_message_content_with_agent_name(
+            agent_name='recognizer',
+            original_content=recognizer_response.content,
+        )
+        # 构建chat-history
+        shared_chat_history = [HumanMessage(content=arbiter_first_message_context)]
+        return shared_chat_history
 
