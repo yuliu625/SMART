6567437f96678fd84bd6aadf6d83cda07cbd670e
yu <yul1024@outlook.com>
2025-06-15 16:22:33 +0800

    Update recognizer.py

--- mas/nodes/decision_agents/recognizer.py
+++ mas/nodes/decision_agents/recognizer.py
@@ -23,6 +23,14 @@ class Recognizer(BaseAgent):
     整体公司风险识别者。
 
     长文档阅读能力。
+
+    进行:
+        - 读取原始文本内容，识别公司架构。
+        - 初步识别风险，分派验证任务。
+
+    架构:
+        - 在MAS中的decision模块。
+        - 在整个MAS开始，仅运行一次。
     """
     def __init__(
         self,
@@ -43,17 +51,41 @@ class Recognizer(BaseAgent):
         state: DecisionState,
         config: RunnableConfig,
     ) -> dict:
+        """
+        由于recognizer为初始化运行且仅运行一次，recognizer不专门设置chat-history，并自己完成chat-history转换过程。
+
+        Args:
+            state (DecisionState): 使用的state。需要字段:
+                - original_pdf_text: 原始文档的文本信息。
+            config (RunnableConfig): 运行设置。
+
+        Returns:
+            dict: 进行更新的字段，包括:
+                - shared_chat_history: 初始化的shared-chat-history。
+                - current_agent_name: 下一个运行的agent。冗余字段，固定边一定指向 'validator'。
+        """
         recognizer_result = self.recognize(original_pdf_text=state.original_pdf_text)
         return {
             'shared_chat_history': recognizer_result.chat_history,
+            'current_agent_name': 'validator',
         }
 
     def recognize(
         self,
         original_pdf_text: str,
     ) -> AgentProcessedResult:
+        """
+        读取原始文档的文本，以decision模块的shared-chat-history返回。
+
+        Args:
+            original_pdf_text (str): 原始文档的文本信息。
+
+        Returns:
+            AgentProcessedResult: 初始化的shared-chat-history。agent_request为空。
+        """
         # 对于初始pdf的文本内容进行分析。
         response = self.call_llm_with_retry(chat_history=[HumanMessage(content=original_pdf_text)])
+        # 获得共享的chat-history。
         shared_chat_history = self.get_initial_shared_chat_history(recognizer_response=response)
         return AgentProcessedResult(
             chat_history=shared_chat_history,
@@ -65,20 +97,21 @@ class Recognizer(BaseAgent):
         recognizer_response: AIMessage,
     ) -> list[AnyMessage]:
         """
-        将封装的方法
+        将recognizer初始识别的结果转换，封装为初始shared-chat-history。
 
         Args:
-            recognizer_response:
+            recognizer_response (AIMessage): recognizer初始识别的结果。
 
         Returns:
-
+            list[AnyMessage]: chat-history。
+                这个专用方法实际返回的是shared-chat-history，为list[HumanMessage]，仅一条初始的human-message。
         """
         # 标注身份。
         arbiter_first_message_context = self.wrap_message_content_with_agent_name(
             agent_name='recognizer',
             original_content=recognizer_response.content,
         )
-        # 构建chat-history
+        # 构建chat-history。
         shared_chat_history = [HumanMessage(content=arbiter_first_message_context)]
         return shared_chat_history
 
