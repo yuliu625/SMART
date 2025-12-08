0f64ca3b015c7c9905be0bf065b04f29aff351c2
yu <yul1024@outlook.com>
2025-12-08 17:32:05 +0800

    Create structured_output_format.py

---
+++ mas/schemas/structured_output_format.py
@@ -0,0 +1,63 @@
+"""
+所有需要进行structured-output的数据类。
+"""
+
+from __future__ import annotations
+from pydantic import BaseModel, Field
+
+from langchain_core.messages import AnyMessage
+
+from typing import Literal
+
+
+# ====RAG====
+class RAGRewriteResponse(BaseModel):
+    items: list[str] = Field(
+        description="multi-query-RAG中，进行query重写的agent生成的结果。",
+        min_length=3, max_length=5,
+    )
+
+
+# ====analysis & decision====
+class AgentRequest(BaseModel):
+    agent_name: Literal[
+        'arbiter', 'validator',
+        'document_reader',
+        'control_analyst', 'financial_analyst', 'strategic_analyst',
+    ] = Field(
+        description="指定下一个运行的agent的名字。"
+    )
+    agent_message: str = Field(
+        description="传递给下一个的agent的信息。"
+    )
+
+
+class AgentProcessedResult(BaseModel):
+    """
+
+    """
+    chat_history: list[AnyMessage] = Field(
+        description="",
+    )
+    agent_request: AgentRequest | None = Field(
+        description="",
+    )
+
+
+# ====decision====
+class ArbiterDecision(BaseModel):
+    decision: str = Field(
+        description="最终的决定。"
+    )
+    risk: bool = Field(
+        description="是否存在风险的判断。"
+    )
+    confidence: float = Field(
+        description="对于仲裁结果的置信度。"
+    )
+
+
+class ConfidenceOutput(BaseModel):
+    content: str
+    confidence: float
+
