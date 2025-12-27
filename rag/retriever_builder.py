78987fcc40fe873d8f9caf6019628c5ba4dda9e8
yu <yul1024@outlook.com>
2025-12-27 19:15:23 +0800

    Create retriever_builder.py

---
+++ rag/retriever_builder.py
@@ -0,0 +1,17 @@
+"""
+构造可直接使用的retriever。
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class RetrieverBuilder:
+    @staticmethod
+    def build_retriever(
+
+    ):
+        ...
+
