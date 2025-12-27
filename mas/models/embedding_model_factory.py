7720127eeda3c94bbb05970ed5b9851d958fa784
yu <yul1024@outlook.com>
2025-12-27 19:15:45 +0800

    Update embedding_model_factory.py

--- mas/models/embedding_model_factory.py
+++ mas/models/embedding_model_factory.py
@@ -4,5 +4,16 @@
 
 from __future__ import annotations
 
+from langchain_core.embeddings import Embeddings
+
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
+
+
+class EmbeddingModelFactory:
+    @staticmethod
+    def create_embedding_model(
+
+    ) -> Embeddings:
+        ...
+
