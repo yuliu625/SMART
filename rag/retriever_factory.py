a497e6e9fbae82784018edc8327adfce9bfd698a
yu <yul1024@outlook.com>
2025-12-27 19:15:25 +0800

    Create retriever_factory.py

---
+++ rag/retriever_factory.py
@@ -0,0 +1,17 @@
+"""
+retriever的工厂。
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class RetrieverFactory:
+    @staticmethod
+    def create_retriever(
+
+    ):
+        ...
+
