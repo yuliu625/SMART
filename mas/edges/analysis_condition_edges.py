505033bd205379dd897bce123b4439dd39d8243f
yu <yul1024@outlook.com>
2025-12-27 19:15:39 +0800

    Create analysis_condition_edges.py

---
+++ mas/edges/analysis_condition_edges.py
@@ -0,0 +1,17 @@
+"""
+分析模块会使用的条件边。
+"""
+
+from __future__ import annotations
+
+from mas.schemas.mas_state import MASState
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+def is_need_more_information(
+    state: MASState,
+) -> bool:
+    ...
+
