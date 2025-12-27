345151daba55079d8b275a829216beaf43a51086
yu <yul1024@outlook.com>
2025-12-27 19:15:28 +0800

    Create vector_store_factory.py

---
+++ rag/vector_store_factory.py
@@ -0,0 +1,17 @@
+"""
+构建vector store的方法。
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class VectorStoreFactory:
+    @staticmethod
+    def create_vector_store(
+
+    ):
+        ...
+
