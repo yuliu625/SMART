94769acae911d3c0112d0997d8e0a32fe8a7905b
yu <yul1024@outlook.com>
2025-12-29 20:02:23 +0800

    Update retriever_factory.py

--- rag/retriever_factory.py
+++ rag/retriever_factory.py
@@ -4,6 +4,8 @@ retriever的工厂。
 
 from __future__ import annotations
 
+# from langchain.retrievers import Retriever
+
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from langchain_core.vectorstores import VectorStore, VectorStoreRetriever
@@ -14,5 +16,9 @@ class RetrieverFactory:
     def create_retriever(
         vector_store: VectorStore,
     ) -> VectorStoreRetriever:
-        ...
+        retriever = vector_store.as_retriever(
+            search_type='mmr',
+            search_kwargs={'k': 3, 'filter': {}},
+        )
+        return retriever
 
