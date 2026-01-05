026b445cf8d6ec844dd0d31f1c34f05988284a35
yu <yul1024@outlook.com>
2026-01-05 16:03:31 +0800

    feat: add matching sample selection related methods

---
+++ data_processing/pre_analysis/select_matching_sample.py
@@ -0,0 +1,17 @@
+"""
+选择配对样本的方法。
+"""
+
+from __future__ import annotations
+
+import pandas as pd
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+def select_matching_sample(
+    df: pd.DataFrame,
+) -> pd.DataFrame:
+    ...
+
