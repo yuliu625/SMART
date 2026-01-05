dd22118a87c316aad7804c77695362469339428b
yu <yul1024@outlook.com>
2026-01-05 16:13:33 +0800

    docs: add docstring

--- data_processing/pre_analysis/select_a_share_companies.py
+++ data_processing/pre_analysis/select_a_share_companies.py
@@ -17,10 +17,10 @@ def select_a_share_companies_by_market_type(
     通过市场类型选择，选择A股公司。
 
     Args:
-        df:
+        df (pd.DataFrame): 符合CSMAR字段设置的数据集。需要提前的预处理。
 
     Returns:
-
+        pd.DataFrame: 筛选后的数据集。
     """
     df_ = df[df['Markettype'].isin(['1', '4', '16', '32', '64'])]
     return df_
@@ -35,10 +35,10 @@ def select_a_share_companies_by_currency(
     这个方法作为辅助验证，因为B股可能以其他货币进行交易。
 
     Args:
-        df:
+        df (pd.DataFrame): 符合CSMAR字段设置的数据集。需要提前的预处理。
 
     Returns:
-
+        pd.DataFrame: 筛选后的数据集。
     """
     df_ = df[df['Curtrd'] == 'CNY']
     return df_
