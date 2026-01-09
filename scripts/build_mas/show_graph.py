032e359fc828a2cda6bcf4ab500f58a3780007e6
yu <yul1024@outlook.com>
2026-01-09 21:14:23 +0800

    feat: add method to check graph in visual way

---
+++ scripts/build_mas/show_graph.py
@@ -0,0 +1,20 @@
+"""
+可视化检查当前计算图的方法。
+"""
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from langgraph.graph.state import CompiledStateGraph
+
+def main(
+
+):
+    ...
+
+
+if __name__ == '__main__':
+    graph_: CompiledStateGraph
+    main()
+
