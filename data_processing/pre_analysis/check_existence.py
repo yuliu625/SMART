c256052a2130aa3032164d4faf1524cbd64be3dd
yu <yul1024@outlook.com>
2026-01-10 16:09:25 +0800

    feat: add method to check existence of pdf files

--- data_processing/pre_analysis/check_existence.py
+++ data_processing/pre_analysis/check_existence.py
@@ -13,7 +13,17 @@ if TYPE_CHECKING:
 
 
 def check_original_pdf_existence(
-    file_type: str,
-):
-    ...
+    target_name_list: list[str],
+    pdf_dir: str | Path,
+) -> None:
+    # 匹配所有一级目录下的pdf文件。
+    pdf_path_list: list[Path] = list(
+        Path(pdf_dir).rglob("*.pdf")
+    )
+    # 获取pdf_name，只需要前6位stock code。
+    pdf_name_list: list[str] = [pdf_path.stem[:6] for pdf_path in pdf_path_list]
+    # 遍历所有的目标文件，查询是否在本地实际文件中存在。
+    for target_name in target_name_list:
+        if target_name not in pdf_name_list:
+            logger.warning(f"PDF file {target_name} not exist.")
 
