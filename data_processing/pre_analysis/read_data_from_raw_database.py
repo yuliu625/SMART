27d980d731bceceb562ae56d24b68b5cb386d9b0
yu <yul1024@outlook.com>
2026-01-10 17:00:21 +0800

    feat: add method to read processed csmar xlsx file

--- data_processing/pre_analysis/read_data_from_raw_database.py
+++ data_processing/pre_analysis/read_data_from_raw_database.py
@@ -32,3 +32,25 @@ def read_xlsx_from_csmar_trd_co(
     logger.trace("CSMAR TRD Co file: \n", csmar_trd_co_xlsx_file_path)
     return df
 
+
+def read_processed_xlsx_from_csmar_trd_co(
+    csmar_trd_co_xlsx_file_path: str,
+) -> pd.DataFrame:
+    """
+    读取从CSMAR数据的xlsx数据。
+
+    Args:
+        csmar_trd_co_xlsx_file_path (str): CSMAR导出的xlsx数据集的路径。
+
+    Returns:
+        pd.DataFrame: 可以正常处理的df。
+    """
+    logger.debug(f"CSMAR TRD Co file path: {csmar_trd_co_xlsx_file_path}")
+    df = pd.read_excel(
+        csmar_trd_co_xlsx_file_path,
+        # skiprows=[1, 2, ],  # 去除2行column说明。
+        dtype=str,  # 禁用自动类型推断。全部column都为str。
+    )
+    logger.trace("CSMAR TRD Co file: \n", csmar_trd_co_xlsx_file_path)
+    return df
+
