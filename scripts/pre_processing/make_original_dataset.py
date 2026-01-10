4c5740af61108cdbf4fd64f554c8b2693121a827
yu <yul1024@outlook.com>
2026-01-10 16:58:33 +0800

    feat: add script to make original dataset

--- scripts/pre_processing/make_original_dataset.py
+++ scripts/pre_processing/make_original_dataset.py
@@ -23,13 +23,17 @@ def make_target_companies_in_2024(
     csmar_trd_co_df = read_xlsx_from_csmar_trd_co(
         csmar_trd_co_xlsx_file_path=csmar_trd_co_xlsx_file_path,
     )
+    # 选择A股公司。
     df = select_a_share_companies_by_market_type(
         df=csmar_trd_co_df,
     )
+    # 选择处于交易状态的公司。
     df = select_active_companies(
         df=df,
     )
+    # 保存。
     df.to_excel(result_path, index=False)
+    # 这里的保存以及不再有多于的字段说明记录。
     logger.success(f"Saved {result_path}")
 
 
@@ -40,5 +44,19 @@ def main(
 
 
 if __name__ == '__main__':
-    csmar_trd_co_file_path_ = r""
+    # 处理全部公司的数据。
+    csmar_trd_co_new_file_path_ = r"D:\dataset\smart\original_data\csmar\TRD_Co_new.xlsx"
+    all_companies_path_ = r"D:\dataset\smart\original_data\csmar\all_companies_in_2024.xlsx"
+    make_target_companies_in_2024(
+        csmar_trd_co_xlsx_file_path=csmar_trd_co_new_file_path_,
+        result_path=all_companies_path_,
+    )
+
+    # 处理ST公司的数据。
+    csmar_trd_co_st_file_path_ = r"D:\dataset\smart\original_data\csmar\TRD_Co_st.xlsx"
+    st_companies_path_ = r"D:\dataset\smart\original_data\csmar\st_companies_in_2024.xlsx"
+    make_target_companies_in_2024(
+        csmar_trd_co_xlsx_file_path=csmar_trd_co_st_file_path_,
+        result_path=st_companies_path_,
+    )
 
