8d6959fb43b709b02ff6357c5b8540ed3aa4f9b1
yu <yul1024@outlook.com>
2026-01-12 18:55:10 +0800

    feat: add log

--- data_processing/utils/copy_pdf.py
+++ data_processing/utils/copy_pdf.py
@@ -17,11 +17,17 @@ def copy_pdf_by_csmar_stock_code(
     source_dir: str | Path,
     target_dir: str | Path,
 ) -> None:
+    # 路径相关预处理。
     source_dir = Path(source_dir)
     target_dir = Path(target_dir)
     target_dir.mkdir(parents=True, exist_ok=True)
-
+    logger.debug(f"Source_dir: {source_dir}. Target_dir: {target_dir}")
+    logger.trace(f"target_pdf_name_list={target_pdf_name_list}")
+    # 逐一处理每个文件。
     for pdf_name in target_pdf_name_list:
+        # 正则查找每个文件。
         found_files = list(source_dir.glob(f"{pdf_name}*"))
+        # 实际能找到一个，进行复制。
         shutil.copy2(found_files[0], target_dir / f"{pdf_name}.pdf")
+        logger.debug(f"Copied {pdf_name}.pdf")
 
