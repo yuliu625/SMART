3990a6da259acc379745798a0322036077e40adb
yu <yul1024@outlook.com>
2026-01-16 20:10:03 +0800

    feat: add script to make mardown dir

--- scripts/pdf_process/make_pymupdf_dir.py
+++ scripts/pdf_process/make_pymupdf_dir.py
@@ -15,3 +15,49 @@ from pathlib import Path
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
 
+
+def batch_convert_pdf_via_pymupdf4llm(
+    pdf_dir: str | Path,
+    result_dir: str | Path,
+) -> None:
+    # 路径处理。
+    result_dir = Path(result_dir)
+    result_dir.mkdir(parents=True, exist_ok=True)
+    # 批量处理。
+    pdf_dir = Path(pdf_dir)
+    # 以所有的pdf文件为目标。
+    ## HARDCODED: 默认方法是基于路径自动处理，无关指定目标文件。
+    pdf_path_list = list(pdf_dir.glob('*.pdf'))
+    for _pdf_path in pdf_path_list:
+        # 命名方式: 同stem，仅修改文件后缀。
+        _result_markdown_path = result_dir / f"{_pdf_path.stem}.md"
+        convert_pdf_via_pymupdf4llm(
+            pdf_path=_pdf_path,
+            result_markdown_path=_result_markdown_path,
+        )
+    logger.success(f"Convert {pdf_dir} to {result_dir}.")
+
+
+def batch_convert_pdf_via_pymupdf4llm_with_images(
+    pdf_dir: str | Path,
+    result_dir: str | Path,
+) -> None:
+    ...
+    # 命名规则: 同级文件夹下，新建同名文件夹保存images。
+
+
+def main(
+
+) -> None:
+    ...
+
+
+if __name__ == '__main__':
+    # 最基础的转换。
+    pdf_dir_ = r""
+    result_dir_ = r""
+    batch_convert_pdf_via_pymupdf4llm(
+        pdf_dir=pdf_dir_,
+        result_dir=result_dir_,
+    )
+
