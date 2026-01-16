e8a4e22033bbcb376d5ce489e39c0b9f13cfb71e
yu <yul1024@outlook.com>
2026-01-16 20:22:10 +0800

    feat: add script to make markdown dir

--- scripts/pdf_process/make_docling_dir.py
+++ scripts/pdf_process/make_docling_dir.py
@@ -6,12 +6,32 @@ from __future__ import annotations
 from loguru import logger
 
 from data_processing.pdf_processing.convert_pdf_via_docling import (
-    set_pdf_pipeline,
+    build_pdf_pipeline_options,
     convert_pdf_via_docling,
+    batch_convert_pdf_via_docling,
 )
 
 from pathlib import Path
 
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
+
+
+def default_batch_conver_pdf_via_docling(
+    pdf_path: str | Path,
+    result_dir: str | Path,
+) -> None:
+    ...
+
+
+def main(
+
+):
+    ...
+
+
+if __name__ == '__main__':
+    # 默认转换方法。
+    pdf_dir_ = r""
+    result_dir_ = r""
 
