9b6870bf2637dcfd7203fdbb8f9006b910f45722
yu <yul1024@outlook.com>
2026-01-18 21:01:29 +0800

    feat: add script to make markdown dir

--- scripts/pdf_process/make_pymupdf_dir.py
+++ scripts/pdf_process/make_pymupdf_dir.py
@@ -7,7 +7,7 @@ from loguru import logger
 
 from data_processing.pdf_processing.convert_pdf_via_pymupdf4llm import (
     convert_pdf_via_pymupdf4llm,
-    convert_pdf_via_pymupdf4llm_with_images
+    convert_pdf_via_pymupdf4llm_with_images,
 )
 
 from pathlib import Path
