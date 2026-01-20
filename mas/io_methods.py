fbe24ef582050e2999b43487ce7eeff0ae18729c
yu <yul1024@outlook.com>
2026-01-20 17:31:57 +0800

    feat: add io methods

---
+++ mas/io_methods.py
@@ -0,0 +1,18 @@
+"""
+对于当前MAS的IO方法。
+
+针对mas.schemas中的定义。
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from pathlib import Path
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class IOMethods:
+    ...
+
