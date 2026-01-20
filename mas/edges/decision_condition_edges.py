8d8fc61ab252f9c7eac8f9523218cbbe7e30a3e0
yu <yul1024@outlook.com>
2026-01-20 15:32:21 +0800

    refactor: rename

--- mas/edges/decision_condition_edges.py
+++ mas/edges/decision_condition_edges.py
@@ -4,20 +4,20 @@
 
 from __future__ import annotations
 
-from mas.schemas.final_mas_state import MASState
+from mas.schemas.final_mas_state import FinalMASState
 
 from typing import TYPE_CHECKING, Literal
 # if TYPE_CHECKING:
 
 
 def is_need_validation(
-    state: MASState,
+    state: FinalMASState,
 ) -> bool:
     ...
 
 
 def get_next_agent_name(
-    state: MASState,
+    state: FinalMASState,
 ) -> Literal['investigator', 'analyst']:
     ...
 
