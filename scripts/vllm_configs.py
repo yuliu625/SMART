7faeeed47d6b4408afd2fa8607e76ae2a23a8f89
yu <yul1024@outlook.com>
2026-01-20 21:14:54 +0800

    feat: add new vllm configs

--- scripts/vllm_configs.py
+++ scripts/vllm_configs.py
@@ -9,4 +9,18 @@ from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
 
 
+# Qwen/Qwen2.5-7B-Instruct-1M
+# http://localhost:12345/v1/chat/completions
+dict(
+    mode='serve',
+    model=r'/home/liuyu/liuyu_nfs_data/model/Qwen/Qwen2.5-7B-Instruct-1M',
+    host='127.0.0.1',
+    port=12345,
+    gpu_memory_utilization=0.1,
+    tensor_parallel_size=4,
+    max_model_len=600000,
+    max_num_batched_tokens=131072,
+    max_num_seqs=1,
+    lora_modules=None,
+)
 
