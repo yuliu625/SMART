635c4f2d2ecbc4bf46efe484aca39cd2bcda73d8
yu <yul1024@outlook.com>
2026-01-20 14:43:53 +0800

    feat: add new graph

--- mas/graphs/sequential_mas_graph_builder.py
+++ mas/graphs/sequential_mas_graph_builder.py
@@ -1,11 +1,17 @@
 """
-简单的MAS design pattern，基于固定序列流。
+design pattern: 基于固定序列流。
+
+接近于最终MAS中的仅decision module。
 """
 
 from __future__ import annotations
+from loguru import logger
 
-from mas.schemas.mas_state import MASState # 由于构建graph_builder需要使用到MASState，因此不能仅以类型声明。
-from mas.agent_nodes.agent_factory import AgentFactory
+from mas.schemas.single_agent_mas_state import SingleAgentMASState
+from mas.agent_nodes.decision_agents.surveyor import Surveyor
+from mas.agent_nodes.decision_agents.investigator import Investigator
+from mas.agent_nodes.decision_agents.adjudicator import Adjudicator
+# from mas.agent_nodes.agent_factory import AgentFactory
 
 from langgraph.graph import (
     StateGraph,
@@ -25,7 +31,7 @@ class SequentialMASGraphBuilder:
     """
     def __init__(
         self,
-        state: type[MASState],
+        state: type[SingleAgentMASState],
     ):
         # 初始化计算图构建。实际中不会以变量传入state，因为state为数据类，更多实现方法为以包导入并写死。
         self.graph_builder = StateGraph(state)
@@ -33,21 +39,34 @@ class SequentialMASGraphBuilder:
 
     def build_graph(
         self,
+        surveyor: Surveyor,
+        investigator: Investigator,
+        adjudicator: Adjudicator,
         checkpointer: BaseCheckpointSaver | None = None,
     ) -> CompiledStateGraph:
-        self._add_nodes()
+        self._add_nodes(
+            surveyor=surveyor,
+            investigator=investigator,
+            adjudicator=adjudicator,
+        )
         self._add_edges()
         graph = self.graph_builder.compile(checkpointer=checkpointer)
+        logger.info(f"Built sequential mas graph.")
         return graph
 
-    def _add_nodes(self):
+    def _add_nodes(
+        self,
+        surveyor: Surveyor,
+        investigator: Investigator,
+        adjudicator: Adjudicator,
+    ):
         """
         注册MAS的nodes。
         """
         # Decision Module
-        surveyor = AgentFactory.create_surveyor()
-        investigator = AgentFactory.create_investigator()
-        adjudicator = AgentFactory.create_adjudicator()
+        # surveyor = AgentFactory.create_surveyor()
+        # investigator = AgentFactory.create_investigator()
+        # adjudicator = AgentFactory.create_adjudicator()
         self.graph_builder.add_node('surveyor', surveyor.process_state)
         self.graph_builder.add_node('investigator', investigator.process_state)
         self.graph_builder.add_node('adjudicator', adjudicator.process_state)
@@ -56,7 +75,9 @@ class SequentialMASGraphBuilder:
         """
         注册MAS的edges。
         """
+        # 输入的内容由surveyor进行处理。
         self.graph_builder.add_edge(START, 'surveyor')
         self.graph_builder.add_edge('surveyor', 'investigator')
+        # Adjudicator读取全部的信息，并做出最终的判断。
         self.graph_builder.add_edge('adjudicator', END)
 
