ccda3747ccf5fea1649d9d63d061145175e1bfb8
yu <yul1024@outlook.com>
2026-01-20 16:09:22 +0800

    to: add new mas

--- mas/mas_factory.py
+++ mas/mas_factory.py
@@ -7,5 +7,40 @@
 from __future__ import annotations
 from loguru import logger
 
+# Graphs
+from mas.graphs.graph_factory import GraphFactory
+# LLM
+from mas.models.local_llm_factory import LocalLLMFactory
+# Prompts
+from mas.prompts.prompt_template_loader import PromptTemplateLoader
+
 from typing import TYPE_CHECKING
-# if TYPE_CHECKING:
+if TYPE_CHECKING:
+    from langgraph.graph.state import CompiledStateGraph
+
+
+class MASFactory:
+    @staticmethod
+    def create_single_agent_mas(
+
+    ) -> CompiledStateGraph:
+        raise NotImplementedError
+
+    @staticmethod
+    def create_sequential_mas(
+
+    ) -> CompiledStateGraph:
+        raise NotImplementedError
+
+    @staticmethod
+    def create_multi_agent_debate_mas(
+
+    ) -> CompiledStateGraph:
+        raise NotImplementedError
+
+    @staticmethod
+    def create_final_mas(
+
+    ) -> CompiledStateGraph:
+        raise NotImplementedError
+
