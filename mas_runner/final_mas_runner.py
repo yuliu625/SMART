569dbd0f00d798ba46d1bc99cc0f00d18d7369ff
yu <yul1024@outlook.com>
2026-01-21 19:02:11 +0800

    feat: add new runner

---
+++ mas_runner/final_mas_runner.py
@@ -0,0 +1,14 @@
+"""
+实际构建MAS的自动运行。
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from mas.mas_factory import MASFactory
+from mas.io_methods import IOMethods
+
+from pathlib import Path
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
