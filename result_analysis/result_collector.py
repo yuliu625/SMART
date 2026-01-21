95fc28855db993264b7c2d38d4a83231da2a4dab
yu <yul1024@outlook.com>
2026-01-21 18:15:56 +0800

    feat: add methods to collect results

---
+++ result_analysis/result_collector.py
@@ -0,0 +1,22 @@
+"""
+结果收集工具。
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from pathlib import Path
+import json
+import pandas as pd
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class ResultCollector:
+    @staticmethod
+    def collect_decision_results(
+
+    ):
+        ...
+
