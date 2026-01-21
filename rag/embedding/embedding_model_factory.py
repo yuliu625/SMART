ab62a36d4dcf00ed0b78054924b3f218290b23e3
yu <yul1024@outlook.com>
2026-01-21 20:44:05 +0800

    feat: add embedding model factory

--- rag/embedding/embedding_model_factory.py
+++ rag/embedding/embedding_model_factory.py
@@ -12,9 +12,11 @@ Notes:
 """
 
 from __future__ import annotations
+from loguru import logger
 
 from langchain_huggingface import HuggingFaceEmbeddings
-import os
+from langchain_ollama import OllamaEmbeddings
+from langchain_openai import OpenAIEmbeddings
 
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
@@ -24,17 +26,92 @@ if TYPE_CHECKING:
 class EmbeddingModelFactory:
     """
     基础的embedding-model-factory。
+
+    在目前的langchain体系中，embedding-model是仅基于文本的单模态模型。
     """
     @staticmethod
-    def create_huggingface_embedding_model(
-
+    def create_ollama_embedding_model(
+        model_name: str,
+        repeat_penalty: float | None,
+        temperature: float | None,
+        stop_tokens: list[str] | None,
+        top_k: int | None,
+        top_p: float | None,
     ) -> Embeddings:
-        raise NotImplementedError
+        """
+        基于Ollama中的OllamaEmbeddings的embedding-model。
+
+        ollama已经有很好的实现和推理，这里仅指出重要args。
+
+        Args:
+            model_name:
+            repeat_penalty:
+            temperature:
+            stop_tokens:
+            top_k:
+            top_p:
+
+        Returns:
+            Embeddings:
+        """
+        embedding_model = OllamaEmbeddings(
+            model=model_name,
+            repeat_penalty=repeat_penalty,
+            temperature=temperature,
+            stop=stop_tokens,
+            top_k=top_k,
+            top_p=top_p,
+        )
+        return embedding_model
 
     @staticmethod
-    def create_multi_modal_embedding_model(
-
+    def create_huggingface_embedding_model(
+        model_name_or_path: str,
+        model_kwargs: dict,
+        encode_kwargs: dict,
+        query_encode_kwargs: dict,
+        is_multi_process: bool,
+        cache_folder: str | None,
+        is_show_progress: bool,
     ) -> Embeddings:
+        """
+        构造基于HuggingFaceEmbeddings的embedding-model。
+
+        Notes:
+            - HuggingFaceEmbeddings支持多推理后端，但约定最好使用sentence-transformers。
+            - 社区实现基于BaseModel限制，因此我在该方法中显式指定了所有参数。
+                如有需要，查看HuggingFaceEmbeddings源代码。
+
+        Args:
+            model_name_or_path:
+            model_kwargs:
+            encode_kwargs:
+            query_encode_kwargs:
+            is_multi_process (bool): 是否将encode方法在多张GPU上运行。
+            cache_folder:
+            is_show_progress (bool): 是否显示展示编码进度的progress bar。
+
+        Returns:
+            Embeddings:
+        """
+        embedding_model = HuggingFaceEmbeddings(
+            model_name=model_name_or_path,
+            model_kwargs=model_kwargs,
+            encode_kwargs=encode_kwargs,
+            query_encode_kwargs=query_encode_kwargs,
+            multi_process=is_multi_process,
+            cache_folder=cache_folder,
+            is_show_progress=is_show_progress,
+        )
+        return embedding_model
+
+    @staticmethod
+    def create_openai_embeddings_model(
+        model_name: str,
+    ) -> Embeddings:
+        embedding_model = OpenAIEmbeddings(
+            model=model_name,
+        )
         raise NotImplementedError
 
 
