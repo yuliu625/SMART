eee24544e4e3661619297fdae4b010bc825e1011
yu <yul1024@outlook.com>
2026-01-21 20:11:29 +0800

    fix: fix unified state

--- mas/schemas/final_mas_state.py
+++ mas/schemas/final_mas_state.py
@@ -16,22 +16,28 @@ class FinalMASState(BaseModel):
     最终构建的MAS的state。
     """
     # MAS条件控制。
-    # current related, 整个MAS控制相关。
+    ## current related, 整个MAS控制相关。
     current_agent_name: str = Field(
-        default="arbiter",
-        description="当前执行操作的agent的名字",
+        default="adjudicator",
+        description="当前应该运行的agent的名字。",
     )
     current_message: str = Field(
         default="",
-        description="调用当前agent的上一个agent传递的信息。",
+        description="当前agent可能会使用的信息。",
     )
 
     # Decision Module
+    ## 共用字段。
     decision_shared_messages: list[AnyMessage] = Field(
         default_factory=list,
         description="所有decision中的agent共用的messages。",
     )
-    ## Surveyor
+    ## Adjudicator. 代表最终结果的重要字段。
+    final_decision: dict = Field(
+        default_factory=dict,
+        description="Adjudicator结构化输出的最终决策。",
+    )
+    ## Surveyor. 启动。
     original_pdf_text: str = Field(
         description="原始的pdf文档的text。专门用一个字段是为了避免和current_message出现问题。",
     )
@@ -41,50 +47,45 @@ class FinalMASState(BaseModel):
         default=10,
         description="Investigator剩余可进行验证的次数。",
     )
-    more_information: bool = Field(
+    is_more_information: bool = Field(
         default=False,
         description="Analyst选择是否需要进一步的分析。",
     )
-    ## validator要求analysis模块要进行重新分析的{agent: content}。
+    ## Investigator要求analysis模块要进行重新分析的{agent: content}。
     is_need_verification: bool = Field(
         default=False,
         description="Investigator决定是否需要Analyst进一步分析。",
     )
-    ## Adjudicator
-    final_decision: dict = Field(
-        default_factory=dict,
-        description="Adjudicator结构化输出的最终决策。",
-    )
 
     # Analysis Module
+    ## Analyst
     remaining_retrieve_rounds: int = Field(
         default=5,
         description="analyst剩余可以进行查询次数。",
     )
-    last_round_result_number: int = Field(
-        default=-1,
-        description="上轮获取的结果的数量",
-    )
-    query_chat_history: list[AnyMessage] = Field(
-        default_factory=list,
-        description="query重写使用。查询的历史，用于rag中的agent进行对话。",
-    )
-    query_result_history: list[Document] = Field(
-        default_factory=list,
-        description="历史查询的结果，返回新查询结果是用于去重。",
-    )
-    ## query result
-    current_query_results: list[Document] = Field(
-        default_factory=list,
-        description="当前轮返回的最终结果。",
-    )
-    # analysis agents
-    document_reader_chat_history: list[AnyMessage] = Field(
-        default_factory=list,
-        description="文档阅读的历史",
-    )
     analyst_chat_history: list[AnyMessage] = Field(
         default_factory=list,
-        description="analyst过去获得的信息和进行的分析",
+        description="Analyst获得的信息和分析过程。",
     )
+    # query_result_history: list[Document] = Field(
+    #     default_factory=list,
+    #     description="历史查询的结果，返回新查询结果是用于去重。",
+    # )
+    # analysis agents
+    # document_reader_chat_history: list[AnyMessage] = Field(
+    #     default_factory=list,
+    #     description="文档阅读的历史",
+    # )
+
+    # RAG
+    ## 检索结果/当前analyst可以进行分析的材料。
+    current_documents: list[Document] = Field(
+        default_factory=list,
+        description="RAG模块返回的结果。",
+    )
+    ## RAG运行所需要的query由current_message字段复用。
+    # query: str = Field(
+    #     default="",
+    #     description="RAG模块需要执行的查询。"
+    # )
 
