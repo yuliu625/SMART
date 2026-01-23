6fcdddbce223a9a73596062592736ae47bd7175a
yu <yul1024@outlook.com>
2026-01-23 21:35:28 +0800

    test: add tests

--- tests/unit_tests/test_decision_nodes/test_adjudicator.py
+++ tests/unit_tests/test_decision_nodes/test_adjudicator.py
@@ -6,5 +6,19 @@ from __future__ import annotations
 import pytest
 from loguru import logger
 
+from mas.agent_nodes.decision_agents.adjudicator import Adjudicator
+from mas.schemas.final_mas_state import FinalMASState
+from mas.schemas.sequential_mas_state import SequentialMASState
+from mas.schemas.single_agent_mas_state import SingleAgentMASState
+from mas.prompts.prompt_template_loader import PromptTemplateLoader
+
+from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage
+
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
+
+
+class TestAdjudicator:
+    ...
+
