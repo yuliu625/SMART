3b49e3a71c18ef4b22ff2f8664b7fbab3ea60488
yu <yul1024@outlook.com>
2026-01-23 21:35:36 +0800

    test: add tests

--- tests/unit_tests/test_decision_nodes/test_investigator.py
+++ tests/unit_tests/test_decision_nodes/test_investigator.py
@@ -6,5 +6,17 @@ from __future__ import annotations
 import pytest
 from loguru import logger
 
+from mas.agent_nodes.decision_agents.investigator import Investigator
+from mas.schemas.final_mas_state import FinalMASState
+from mas.prompts.prompt_template_loader import PromptTemplateLoader
+
+from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage
+
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
+
+
+class TestInvestigator:
+    ...
+
