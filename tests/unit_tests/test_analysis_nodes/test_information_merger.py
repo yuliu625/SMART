509854c8fea880431573a96a1bcef98c6552261e
yu <yul1024@outlook.com>
2026-01-23 21:35:34 +0800

    test: add tests

---
+++ tests/unit_tests/test_analysis_nodes/test_information_merger.py
@@ -0,0 +1,20 @@
+"""
+Tests for information merger.
+"""
+
+from __future__ import annotations
+import pytest
+from loguru import logger
+
+from mas.agent_nodes.analysis_agents.information_merger import InformationMerger
+from mas.schemas.sequential_mas_state import SequentialMASState
+
+from langchain_core.messages import SystemMessage
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class TestInformationMerger:
+    ...
+
