9445590a3d0363669ab8e451f82b94c209c6f48d
yu <yul1024@outlook.com>
2026-01-23 13:54:00 +0800

    feat: add conditional edges

--- mas/edges/analysis_condition_edges.py
+++ mas/edges/analysis_condition_edges.py
@@ -6,12 +6,25 @@ from __future__ import annotations
 
 from mas.schemas.final_mas_state import FinalMASState
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Literal, cast
 # if TYPE_CHECKING:
 
 
+# ==== for analyst ====
 def is_need_more_information(
     state: FinalMASState,
-) -> bool:
-    ...
+) -> Literal['rag', 'investigation']:
+    """
+    通过直接读取下一个investigator请求的agent，获得下一个agent的路由信息。
+
+    Args:
+        state:
+
+    Returns:
+        Literal['rag', 'investigation']: 下一个agent的名字。
+    """
+    current_agent_name = state.current_agent_name
+    # assert current_agent_name in ('rag', 'investigation')
+    current_agent_name = cast(Literal['rag', 'investigation'], current_agent_name)
+    return current_agent_name
 
