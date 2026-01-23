9b8b81def2042ecfa2492d339a366664facc8ca3
yu <yul1024@outlook.com>
2026-01-23 17:13:01 +0800

    feat: add new agent

---
+++ mas/agent_nodes/analysis_agents/information_merger.py
@@ -0,0 +1,14 @@
+"""
+Information Merger.
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+class InformationMerger:
+    ...
+
