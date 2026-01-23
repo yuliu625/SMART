6918039ff24b187fdfb4402de534942783420475
yu <yul1024@outlook.com>
2026-01-23 21:35:31 +0800

    test: add tests

--- tests/unit_tests/test_analysis_nodes/test_analyst.py
+++ tests/unit_tests/test_analysis_nodes/test_analyst.py
@@ -5,5 +5,18 @@
 from __future__ import annotations
 from loguru import logger
 
+from mas.agent_nodes.analysis_agents.analyst import Analyst
+from mas.schemas.final_mas_state import FinalMASState
+# from mas.models.local_llm_factory import LocalLLMFactory
+from mas.prompts.prompt_template_loader import PromptTemplateLoader
+
+from langchain_ollama import ChatOllama
+from langchain_core.messages import SystemMessage
+
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
+
+
+class TestAnalyst:
+    ...
+
