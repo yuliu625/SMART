a5cef4ddb5b04ec6a49ebd16c3d5976a941e5353
yu <yul1024@outlook.com>
2026-01-23 18:28:15 +0800

    feat: add new mas

--- mas_runner/sequential_mas_runner.py
+++ mas_runner/sequential_mas_runner.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 from loguru import logger
 
 from mas.mas_factory import MASFactory
+from mas.rag_factory import RagFactory
 from mas.io_methods import IOMethods
 
 from pathlib import Path
