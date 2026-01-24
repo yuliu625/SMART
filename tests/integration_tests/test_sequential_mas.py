9887400c9725f55deefc97904026c58373523dd3
yu <yul1024@outlook.com>
2026-01-24 19:00:41 +0800

    test: add tests

--- tests/integration_tests/test_sequential_mas.py
+++ tests/integration_tests/test_sequential_mas.py
@@ -18,7 +18,7 @@ from typing import TYPE_CHECKING
 
 def make_test_simple_rag():
     rag = RAGFactory.create_simple_rag_via_ollama(
-        vector_store_persist_directory=r"D:\dataset\smart\tests\t_vector_store",
+        vector_store_persist_directory=r"D:\dataset\smart\tests\000004",
         embedding_model_model_name='nomic-embed-text',
         embedding_model_num_ctx=8192,
         search_configs={},
@@ -28,7 +28,7 @@ def make_test_simple_rag():
 
 def make_test_multi_query_rag():
     rag = RAGFactory.create_multi_query_rag_via_ollama_base_on_ollama_llm(
-        vector_store_persist_directory=r"D:\dataset\smart\tests\t_vector_store",
+        vector_store_persist_directory=r"D:\dataset\smart\tests\000004",
         embedding_model_model_name='nomic-embed-text',
         embedding_model_num_ctx=8192,
         search_configs={},
@@ -86,4 +86,62 @@ class TestSequentialMASState:
         )
         logger.info(f"\nDesign Pattern: Sequential Workflow\nMermaid Code: \n{mermaid_code}")
 
+    @pytest.mark.parametrize(
+        "surveyor_main_llm_model_name, surveyor_main_llm_system_message_template_path, adjudicator_main_llm_model_name, adjudicator_main_llm_system_message_template_path, adjudicator_formatter_llm_model_name, adjudicator_formatter_llm_system_message_template_path, rag, markdown_file_path, general_query_path, result_path", [
+        ('qwen2.5:1.5b',
+         r'D:\document\code\paper\SMART\mas\prompts\final_mas\surveyor_main_llm_system_prompt_template.j2',
+         'qwen2.5:1.5b',
+         r'D:\document\code\paper\SMART\mas\prompts\final_mas\adjudicator_main_llm_system_prompt_template.j2',
+         'qwen2.5:1.5b',
+         r'D:\document\code\paper\SMART\mas\prompts\final_mas\adjudicator_formatter_llm_system_prompt_template.j2',
+         make_test_simple_rag(),
+         r"D:\dataset\smart\tests\docling_1\000004.md",
+         r"D:\document\code\paper\SMART\mas\prompts\rag\general_query_human_prompt_template.j2",
+         r"D:\dataset\smart\tests\sequential_workflow_000004.json",
+         ),
+        # ('qwen2.5:1.5b',
+        #  r'D:\document\code\paper\SMART\mas\prompts\final_mas\surveyor_main_llm_system_prompt_template.j2',
+        #  'qwen2.5:1.5b',
+        #  r'D:\document\code\paper\SMART\mas\prompts\final_mas\adjudicator_main_llm_system_prompt_template.j2',
+        #  'qwen2.5:1.5b',
+        #  r'D:\document\code\paper\SMART\mas\prompts\final_mas\adjudicator_formatter_llm_system_prompt_template.j2',
+        #  make_test_multi_query_rag(),
+        #  ),
+    ])
+    @pytest.mark.asyncio
+    async def test_sequential_mas_with_load_and_save(
+        self,
+        # Surveyor
+        surveyor_main_llm_model_name: str,
+        surveyor_main_llm_system_message_template_path: str,
+        # Adjudicator
+        adjudicator_main_llm_model_name: str,
+        adjudicator_main_llm_system_message_template_path: str,
+        adjudicator_formatter_llm_model_name: str,
+        adjudicator_formatter_llm_system_message_template_path: str,
+        # Analyst
+        # RAG
+        rag,
+        markdown_file_path: str,
+        general_query_path: str,
+        result_path: str,
+    ):
+        mas = MASFactory.create_sequential_mas_via_ollama(
+            surveyor_main_llm_model_name=surveyor_main_llm_model_name,
+            surveyor_main_llm_system_message_template_path=surveyor_main_llm_system_message_template_path,
+            adjudicator_main_llm_model_name=adjudicator_main_llm_model_name,
+            adjudicator_main_llm_system_message_template_path=adjudicator_main_llm_system_message_template_path,
+            adjudicator_formatter_llm_model_name=adjudicator_formatter_llm_model_name,
+            adjudicator_formatter_llm_system_message_template_path=adjudicator_formatter_llm_system_message_template_path,
+            rag=rag,
+        )
+        state = IOMethods.load_sequential_mas_state(
+            markdown_file_path=markdown_file_path,
+            general_query_path=general_query_path,
+        )
+        result = await mas.ainvoke(
+            input=state,
+        )
+        logger.info(f"\nMAS Result: \n{result}")
+        IOMethods.save_sequential_mas_state(state=result, result_path=result_path)
 
