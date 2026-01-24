dea9fe3a61f8e9f641f432b28db7f08f30505dcb
yu <yul1024@outlook.com>
2026-01-24 19:08:07 +0800

    feat: add new io methods

--- mas/io_methods.py
+++ mas/io_methods.py
@@ -11,6 +11,7 @@ from mas.schemas.single_agent_mas_state import SingleAgentMASState
 from mas.schemas.sequential_mas_state import SequentialMASState
 from mas.schemas.final_mas_state import FinalMASState
 # from rag.loading.load_text import TextLoadingMethods
+from mas.prompts.prompt_template_loader import PromptTemplateLoader
 
 from pathlib import Path
 import json
@@ -34,13 +35,13 @@ class IOMethods:
         # 读取文本。
         markdown_text = markdown_file_path.read_text(encoding='utf-8')
         # 构造state。
-        state = SingleAgentMASState(
+        single_agent_state = SingleAgentMASState(
             decision_shared_messages=[
                 HumanMessage(content=markdown_text),
             ],
         )
-        logger.trace(f"\nLoaded SingleAgentMASState: \n{state}")
-        return state
+        logger.trace(f"\nLoaded SingleAgentState: \n{single_agent_state}")
+        return single_agent_state
 
     @staticmethod
     def save_single_agent_mas_state(
@@ -57,28 +58,63 @@ class IOMethods:
             ),
             final_decision=state['final_decision'].model_dump(),
         )
-        logger.debug(f"Type of result: {type(result)}")
-        logger.debug(f"Type of final_decision: {type(result['final_decision'])}")
-        logger.trace(f"\nSingleAgentMASState to save: \n{result}")
+        # logger.debug(f"Type of result: {type(result)}")
+        # logger.debug(f"Type of final_decision: {type(result['final_decision'])}")
+        logger.trace(f"\nSingleAgentState to save: \n{result}")
         # 执行保存。
         result_path.write_text(
             json.dumps(result, ensure_ascii=False, indent=4),
             encoding='utf-8',
         )
-        logger.success(f"\nSaved SingleAgentMASState: \n{result}")
+        logger.success(f"\nSaved SingleAgentState: \n{result}")
         return state
 
     @staticmethod
     def load_sequential_mas_state(
-
+        markdown_file_path: str | Path,
+        general_query_path: str | Path,
     ) -> SequentialMASState:
-        raise NotImplementedError
+        # 处理路径。
+        markdown_file_path = Path(markdown_file_path)
+        general_query_path = Path(general_query_path)
+        # 读取文本。
+        markdown_text = markdown_file_path.read_text(encoding='utf-8')
+        query_text = PromptTemplateLoader.load_human_message_prompt_template_from_j2(
+            human_message_prompt_template_path=general_query_path,
+        ).format().content
+        # 构造state。
+        sequential_workflow_state = SequentialMASState(
+            original_pdf_text=markdown_text,
+            current_message=query_text,
+        )
+        logger.trace(f"\nLoaded SequentialWorkflowState: \n{sequential_workflow_state}")
+        return sequential_workflow_state
 
     @staticmethod
     def save_sequential_mas_state(
-
+        state: SequentialMASState,
+        result_path: str | Path,
     ) -> SequentialMASState:
-        raise NotImplementedError
+        # 处理路径。
+        result_path = Path(result_path)
+        result_path.parent.mkdir(parents=True, exist_ok=True)
+        # 读取结果。
+        result = dict(
+            decision_shared_messages=messages_to_dict(
+                messages=state['decision_shared_messages'],
+            ),
+            final_decision=state['final_decision'].model_dump(),
+        )
+        # logger.debug(f"Type of result: {type(result)}")
+        # logger.debug(f"Type of final_decision: {type(result['final_decision'])}")
+        logger.trace(f"\nSequentialWorkflowState to save: \n{result}")
+        # 执行保存。
+        result_path.write_text(
+            json.dumps(result, ensure_ascii=False, indent=4),
+            encoding='utf-8',
+        )
+        logger.success(f"\nSaved SequentialWorkflowState: \n{result}")
+        return state
 
     @staticmethod
     def load_final_mas_state(
