bcef374cffb0caffd5926cbdbc3b15f2b687d70f
yu <yul1024@outlook.com>
2026-01-24 19:39:41 +0800

    feat: add pipeline to build vector store

--- rag/vector_store_pipeline.py
+++ rag/vector_store_pipeline.py
@@ -71,7 +71,49 @@ class ChromaVectorStorePipeline:
 
     @staticmethod
     def make_vector_store_via_huggingface(
+        # loading
         markdown_file_path: str | Path,
+        # splitting
+        headers_to_split_on: list[tuple[str, str]],
+        # embedding
+        hugging_face_embedding_model_model_name_or_path: str,
+        hugging_face_embedding_model_model_kwargs: dict,
+        hugging_face_embedding_model_encode_kwargs: dict,
+        hugging_face_embedding_model_query_encode_kwargs: dict,
+        hugging_face_embedding_model_is_multi_process: bool,
+        # storing
+        vector_store_path: str | Path,
     ):
-        ...
+        # loading
+        document = TextLoadingMethods.load_text(
+            text_path=markdown_file_path,
+            # HARDCODED
+            encoding='utf-8',
+            is_autodetect_encoding=False,
+        )
+        # chunks
+        documents = MarkdownSplittingMethods.split_markdown_by_header(
+            document=document,
+            headers_to_split_on=headers_to_split_on,
+            # HARDCODED
+            is_strip_headers=False,
+        )
+        # embedding model
+        embedding_model = EmbeddingModelFactory.create_huggingface_embedding_model(
+            model_name_or_path=hugging_face_embedding_model_model_name_or_path,
+            model_kwargs=hugging_face_embedding_model_model_kwargs,
+            encode_kwargs=hugging_face_embedding_model_encode_kwargs,
+            query_encode_kwargs=hugging_face_embedding_model_query_encode_kwargs,
+            is_multi_process=hugging_face_embedding_model_is_multi_process,
+            # HARDCODED
+            cache_folder=None,
+            is_show_progress=True,
+        )
+        # storing
+        ChromaVectorStoreBuilder.build_new_vector_store_via_default_method(
+            persist_directory=vector_store_path,
+            embedding_function=embedding_model,
+            documents=documents,
+        )
+        logger.success(f"Vector store built in {vector_store_path}")
 
