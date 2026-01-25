d9e8e2d8ec5ad4616a7a356da10c1a461e030f09
yu <yul1024@outlook.com>
2026-01-25 20:25:55 +0800

    feat: add scripts to make vector store

--- scripts/build_vector_store/make_vector_store_dir.py
+++ scripts/build_vector_store/make_vector_store_dir.py
@@ -110,6 +110,30 @@ def incremental_make_vector_store_dir_via_jina(
     result_dir: str | Path,
 ) -> None:
     raise NotImplementedError
+    # 路径处理。
+    markdown_files_dir = Path(markdown_files_dir)
+    result_dir = Path(result_dir)
+    markdown_files_dir.mkdir(parents=True, exist_ok=True)
+    markdown_file_path_list = list(markdown_files_dir.glob('*.md'))
+    for markdown_file_path in markdown_file_path_list:
+        # 以文件夹管理，因此没有扩展名。
+        result_path = result_dir / f"{markdown_file_path.stem}"
+        if not result_path.exists():
+            ChromaVectorStorePipeline.make_vector_store_via_huggingface(
+                markdown_file_path=markdown_file_path,
+                headers_to_split_on=[
+                    ("#", "Header 1"),
+                    ("##", "Header 2"),
+                    ("###", "Header 3"),
+                    ("####", "Header 4"),
+                ],
+                hugging_face_embedding_model_model_name_or_path=r"/home/liuyu/liuyu_nfs_data/model/jinaai/jina-embeddings-v4",
+                hugging_face_embedding_model_model_kwargs=dict(trust_remote_code=True),
+                hugging_face_embedding_model_encode_kwargs=dict(),
+                hugging_face_embedding_model_query_encode_kwargs=dict(),
+                hugging_face_embedding_model_is_multi_process=False,
+                vector_store_path=result_path,
+            )
 
 
 # ==== 临时构建的工具函数。 ====
@@ -165,6 +189,27 @@ def main() -> None:
         process_function=incremental_make_vector_store_dir_via_nomic,
         markdown_files_dir=r'/home/liuyu/liuyu_nfs_data/pdf_dataset/markdown_1/docling_1',
         result_dir=r'/home/liuyu/liuyu_nfs_data/smart_vector_store/bge/markdown_1/docling_1',
+    )
+
+    # jina
+    ## markdown1
+    ### pymupdf1
+    incremental_make_vector_store_dir(
+        process_function=incremental_make_vector_store_dir_via_jina,
+        markdown_files_dir=r'/home/liuyu/liuyu_nfs_data/pdf_dataset/markdown_1/pymupdf_1',
+        result_dir=r'/home/liuyu/liuyu_nfs_data/smart_vector_store/jina/markdown_1/pymupdf_1',
+    )
+    ### pymupdf2
+    incremental_make_vector_store_dir(
+        process_function=incremental_make_vector_store_dir_via_jina,
+        markdown_files_dir=r'/home/liuyu/liuyu_nfs_data/pdf_dataset/markdown_1/pymupdf_2',
+        result_dir=r'/home/liuyu/liuyu_nfs_data/smart_vector_store/jina/markdown_1/pymupdf_2',
+    )
+    ### docling1
+    incremental_make_vector_store_dir(
+        process_function=incremental_make_vector_store_dir_via_jina,
+        markdown_files_dir=r'/home/liuyu/liuyu_nfs_data/pdf_dataset/markdown_1/docling_1',
+        result_dir=r'/home/liuyu/liuyu_nfs_data/smart_vector_store/jina/markdown_1/docling_1',
     )
 
 
