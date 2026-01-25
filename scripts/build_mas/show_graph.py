b6b391f7d56290e8096eb89bc63990e33a857302
yu <yul1024@outlook.com>
2026-01-25 14:06:37 +0800

    refactor: rename

--- scripts/build_mas/show_graph.py
+++ scripts/build_mas/show_graph.py
@@ -16,11 +16,11 @@ if TYPE_CHECKING:
 
 def show_single_agent_mas_graph():
     # HARDCODED: 仅构建，并不实际运行MAS。
-    mas = MASFactory.create_single_agent_mas_via_ollama(
+    mas = MASFactory.create_single_agent_via_ollama(
         adjudicator_main_llm_model_name='qwen2.5:1.5b',
-        adjudicator_main_llm_system_message_template_path=r'D:\document\code\paper\SMART\mas\prompts\single_agent_mas\adjudicator_main_llm_system_prompt_template.j2',
+        adjudicator_main_llm_system_message_template_path=r'/mas/prompts/single_agent\adjudicator_main_llm_system_prompt_template.j2',
         adjudicator_formatter_llm_model_name='qwen2.5:1.5b',
-        adjudicator_formatter_llm_system_message_template_path=r'D:\document\code\paper\SMART\mas\prompts\single_agent_mas\adjudicator_formatter_llm_system_prompt_template.j2',
+        adjudicator_formatter_llm_system_message_template_path=r'/mas/prompts/single_agent\adjudicator_formatter_llm_system_prompt_template.j2',
     )
     mermaid_code = GraphVisualizer.get_mermaid_code(
         graph=mas,
@@ -29,7 +29,7 @@ def show_single_agent_mas_graph():
 
 
 def show_sequential_mas_graph():
-    mas = MASFactory.create_sequential_mas_via_ollama(
+    mas = MASFactory.create_sequential_workflow_via_ollama(
         # surveyor
         surveyor_main_llm_model_name='qwen2.5:1.5b',
         surveyor_main_llm_system_message_template_path=r'D:\document\code\paper\SMART\mas\prompts\final_mas\surveyor_main_llm_system_prompt_template.j2',
