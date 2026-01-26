0520325d37fafe15a8781395af2d610d8e9fd864
yu <yul1024@outlook.com>
2026-01-27 02:12:11 +0800

    test: add tests

--- tests/integration_tests/test_final_mas.py
+++ tests/integration_tests/test_final_mas.py
@@ -39,14 +39,140 @@ def make_test_multi_query_rag():
 
 
 class TestFinalMas:
+    @pytest.mark.parametrize(
+        "surveyor_main_llm_model_name, surveyor_main_llm_system_message_template_path, investigator_main_llm_model_name, investigator_main_llm_system_message_template_path,investigator_formatter_llm_model_name,investigator_formatter_llm_system_message_template_path,adjudicator_main_llm_model_name,adjudicator_main_llm_system_message_template_path,adjudicator_formatter_llm_model_name,adjudicator_formatter_llm_system_message_template_path,analyst_main_llm_model_name,analyst_main_llm_system_message_template_path,analyst_formatter_llm_model_name,analyst_formatter_llm_system_message_template_path,rag,", [
+        ('qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\surveyor_main_llm_system_prompt_template.j2',
+        'qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\investigator_main_llm_system_prompt_template.j2',
+        'qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\investigator_formatter_llm_system_prompt_template.j2',
+        'qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\adjudicator_main_llm_system_prompt_template.j2',
+        'qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\adjudicator_formatter_llm_system_prompt_template.j2',
+        'qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\analyst_main_llm_system_prompt_template.j2',
+        'qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\analyst_formatter_llm_system_prompt_template.j2',
+         make_test_simple_rag()),
+    ])
     def test_final_mas_structure(
         self,
+        # Surveyor
+        surveyor_main_llm_model_name: str,
+        surveyor_main_llm_system_message_template_path: str,
+        # Investigator
+        investigator_main_llm_model_name: str,
+        investigator_main_llm_system_message_template_path: str,
+        investigator_formatter_llm_model_name: str,
+        investigator_formatter_llm_system_message_template_path: str,
+        # Adjudicator
+        adjudicator_main_llm_model_name: str,
+        adjudicator_main_llm_system_message_template_path: str,
+        adjudicator_formatter_llm_model_name: str,
+        adjudicator_formatter_llm_system_message_template_path: str,
+        # Analyst
+        analyst_main_llm_model_name: str,
+        analyst_main_llm_system_message_template_path: str,
+        analyst_formatter_llm_model_name: str,
+        analyst_formatter_llm_system_message_template_path: str,
+        # RAG
+        rag,
     ) -> None:
-        ...
+        mas = MASFactory.create_final_mas_via_ollama(
+            surveyor_main_llm_model_name=surveyor_main_llm_model_name,
+            surveyor_main_llm_system_message_template_path=surveyor_main_llm_system_message_template_path,
+            investigator_main_llm_model_name=investigator_main_llm_model_name,
+            investigator_main_llm_system_message_template_path=investigator_main_llm_system_message_template_path,
+            investigator_formatter_llm_model_name=investigator_formatter_llm_model_name,
+            investigator_formatter_llm_system_message_template_path=investigator_formatter_llm_system_message_template_path,
+            adjudicator_main_llm_model_name=adjudicator_main_llm_model_name,
+            adjudicator_main_llm_system_message_template_path=adjudicator_main_llm_system_message_template_path,
+            adjudicator_formatter_llm_model_name=adjudicator_formatter_llm_model_name,
+            adjudicator_formatter_llm_system_message_template_path=adjudicator_formatter_llm_system_message_template_path,
+            analyst_main_llm_model_name=analyst_main_llm_model_name,
+            analyst_main_llm_system_message_template_path=analyst_main_llm_system_message_template_path,
+            analyst_formatter_llm_model_name=analyst_formatter_llm_model_name,
+            analyst_formatter_llm_system_message_template_path=analyst_formatter_llm_system_message_template_path,
+            rag=rag,
+        )
+        mermaid_code = GraphVisualizer.get_mermaid_code(
+            graph=mas,
+        )
+        logger.info(f"\nDesign Pattern: MAS\nMermaid Code: \n{mermaid_code}")
 
+    @pytest.mark.parametrize(
+        "surveyor_main_llm_model_name, surveyor_main_llm_system_message_template_path, investigator_main_llm_model_name, investigator_main_llm_system_message_template_path, investigator_formatter_llm_model_name, investigator_formatter_llm_system_message_template_path, adjudicator_main_llm_model_name, adjudicator_main_llm_system_message_template_path, adjudicator_formatter_llm_model_name, adjudicator_formatter_llm_system_message_template_path, analyst_main_llm_model_name, analyst_main_llm_system_message_template_path,analyst_formatter_llm_model_name,analyst_formatter_llm_system_message_template_path, rag, markdown_file_path, result_path", [
+        ('qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\surveyor_main_llm_system_prompt_template.j2',
+        'qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\investigator_main_llm_system_prompt_template.j2',
+        'qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\investigator_formatter_llm_system_prompt_template.j2',
+        'qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\adjudicator_main_llm_system_prompt_template.j2',
+        'qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\adjudicator_formatter_llm_system_prompt_template.j2',
+        'qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\analyst_main_llm_system_prompt_template.j2',
+        'qwen2.5:1.5b',
+        r'D:\document\code\paper\SMART\mas\prompts\final_mas\analyst_formatter_llm_system_prompt_template.j2',
+         make_test_simple_rag(),
+         r"D:\dataset\smart\tests\docling_1\000004.md",
+         r"D:\dataset\smart\tests\final_mas_000004.json",
+         ),
+    ])
     @pytest.mark.asyncio
     async def test_final_mas_with_load_and_save(
         self,
+        # Surveyor
+        surveyor_main_llm_model_name: str,
+        surveyor_main_llm_system_message_template_path: str,
+        # Investigator
+        investigator_main_llm_model_name: str,
+        investigator_main_llm_system_message_template_path: str,
+        investigator_formatter_llm_model_name: str,
+        investigator_formatter_llm_system_message_template_path: str,
+        # Adjudicator
+        adjudicator_main_llm_model_name: str,
+        adjudicator_main_llm_system_message_template_path: str,
+        adjudicator_formatter_llm_model_name: str,
+        adjudicator_formatter_llm_system_message_template_path: str,
+        # Analyst
+        analyst_main_llm_model_name: str,
+        analyst_main_llm_system_message_template_path: str,
+        analyst_formatter_llm_model_name: str,
+        analyst_formatter_llm_system_message_template_path: str,
+        # RAG
+        rag,
+        markdown_file_path: str,
+        result_path: str,
     ) -> None:
-        ...
+        mas = MASFactory.create_final_mas_via_ollama(
+            surveyor_main_llm_model_name=surveyor_main_llm_model_name,
+            surveyor_main_llm_system_message_template_path=surveyor_main_llm_system_message_template_path,
+            investigator_main_llm_model_name=investigator_main_llm_model_name,
+            investigator_main_llm_system_message_template_path=investigator_main_llm_system_message_template_path,
+            investigator_formatter_llm_model_name=investigator_formatter_llm_model_name,
+            investigator_formatter_llm_system_message_template_path=investigator_formatter_llm_system_message_template_path,
+            adjudicator_main_llm_model_name=adjudicator_main_llm_model_name,
+            adjudicator_main_llm_system_message_template_path=adjudicator_main_llm_system_message_template_path,
+            adjudicator_formatter_llm_model_name=adjudicator_formatter_llm_model_name,
+            adjudicator_formatter_llm_system_message_template_path=adjudicator_formatter_llm_system_message_template_path,
+            analyst_main_llm_model_name=analyst_main_llm_model_name,
+            analyst_main_llm_system_message_template_path=analyst_main_llm_system_message_template_path,
+            analyst_formatter_llm_model_name=analyst_formatter_llm_model_name,
+            analyst_formatter_llm_system_message_template_path=analyst_formatter_llm_system_message_template_path,
+            rag=rag,
+        )
+        state = IOMethods.load_final_mas_state(
+            markdown_file_path=markdown_file_path,
+        )
+        result = await mas.ainvoke(
+            input=state,
+            config=dict(recursion_limit=100),
+        )
+        logger.info(f"\nMAS Result: \n{result}")
+        IOMethods.save_final_mas_state(state=result, result_path=result_path)
 
