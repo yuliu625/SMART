b75f3e068d63b4622b2a8f2a261cdcc190b2ea43
yu <yul1024@outlook.com>
2026-01-27 19:06:55 +0800

    fix: fix kwargs error

--- scripts/build_mas/show_graph.py
+++ scripts/build_mas/show_graph.py
@@ -18,9 +18,9 @@ def show_single_agent_mas_graph():
     # HARDCODED: 仅构建，并不实际运行MAS。
     mas = MASFactory.create_single_agent_via_ollama(
         adjudicator_main_llm_model_name='qwen2.5:1.5b',
-        adjudicator_main_llm_system_message_template_path=r'/mas/prompts/single_agent\adjudicator_main_llm_system_prompt_template.j2',
+        adjudicator_main_llm_system_message_template_path=r'D:\document\code\paper\SMART\mas\prompts\single_agent\adjudicator_main_llm_system_prompt_template.j2',
         adjudicator_formatter_llm_model_name='qwen2.5:1.5b',
-        adjudicator_formatter_llm_system_message_template_path=r'/mas/prompts/single_agent\adjudicator_formatter_llm_system_prompt_template.j2',
+        adjudicator_formatter_llm_system_message_template_path=r'D:\document\code\paper\SMART\mas\prompts\single_agent\adjudicator_formatter_llm_system_prompt_template.j2',
     )
     mermaid_code = GraphVisualizer.get_mermaid_code(
         graph=mas,
