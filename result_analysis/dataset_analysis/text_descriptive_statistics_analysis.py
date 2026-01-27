d655c4399115187751e433f7c6731b17fd728fb2
yu <yul1024@outlook.com>
2026-01-27 16:58:49 +0800

    feat: add text processing methods

--- result_analysis/dataset_analysis/text_descriptive_statistics_analysis.py
+++ result_analysis/dataset_analysis/text_descriptive_statistics_analysis.py
@@ -5,6 +5,7 @@
 from __future__ import annotations
 from loguru import logger
 
+import fitz
 from pathlib import Path
 
 from typing import TYPE_CHECKING
@@ -20,5 +21,23 @@ def calculate_markdown_character_length(
         markdown_file_path.stem: len(markdown_file_path.read_text(encoding='utf-8'))
         for markdown_file_path in markdown_file_path_list
     }
+    return stats_dict
+
+
+def calculate_pdf_page_number(
+    target_dir: str | Path,
+) -> dict[str, int]:
+    target_dir = Path(target_dir)
+    pdf_file_path_list = list(target_dir.glob('*.pdf'))
+    stats_dict = {}
+    for pdf_file_path in pdf_file_path_list:
+        doc = fitz.open(pdf_file_path)
+        page_number = doc.page_count
+        doc.close()
+        stats_dict[pdf_file_path.stem] = page_number
+    # stats_dict = {
+    #     pdf_file_path.stem: fitz.open(pdf_file_path).page_count
+    #     for pdf_file_path in pdf_file_path_list
+    # }
     return stats_dict
 
