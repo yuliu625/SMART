a8dab03662630b29c7bb405be93767c342e0b36a
yu <yul1024@outlook.com>
2026-01-27 17:34:19 +0800

    feat: add scripts to run experiments

--- scripts/nohup_scripts.sh
+++ scripts/nohup_scripts.sh
@@ -2,15 +2,18 @@
 cd /home/liuyu/workspace/code/smart
 
 # data processing
+conda activate pdf_env
 nohup python make_pymupdf_dir.py >./pymupdf.log 2>&1 &
 nohup python make_docling_dir.py >./docling.log 2>&1 &
 nohup python make_vlm_dir.py >./vlm.log 2>&1 &
 
 # vector store
+conda activate smart_env
 nohup python make_vector_store_dir.py >./vector_store.log 2>&1 &
 
 
 # experiments
+conda activate smart_env
 nohup python run_single_agent_experiments.py >./single_agent.log 2>&1 &
 nohup python run_sequential_workflow_experiments.py >./sequential_workflow.log 2>&1 &
 nohup python run_final_mas_experiments.py >./final_mas.log 2>&1 &
