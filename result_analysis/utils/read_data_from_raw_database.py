f86ed95a297c585cf4d274e7f9e144dae508ac00
yu <yul1024@outlook.com>
2026-01-27 15:40:48 +0800

    feat: add methods to read data

---
+++ result_analysis/utils/read_data_from_raw_database.py
@@ -0,0 +1,16 @@
+"""
+从原始数据集读取数据的方法。
+
+这里从外部导入使用data_processing.pre_analysis.read_data_from_raw_database.py中的方法。
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from data_processing.pre_analysis.read_data_from_raw_database import (
+    read_processed_xlsx_from_csmar_trd_co,
+    read_processed_xlsx_from_csmar_trd_co,
+)
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
