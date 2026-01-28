98f04f262cdfe78bdddb9252f2b51366c65ca02f
yu <yul1024@outlook.com>
2026-01-28 16:02:02 +0800

    feat: add scripts to collect results

--- scripts/results/collect_experiment_results.py
+++ scripts/results/collect_experiment_results.py
@@ -7,16 +7,33 @@
 from __future__ import annotations
 from loguru import logger
 
+from result_analysis.experiment_results_analysis.quantative_results_maker import (
+    QuantativeResultsMaker,
+)
+
+from pathlib import Path
+
 from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
 
 
-def main():
-    print(ResultCollector.collect_decision_results(target_dir=r"D:\dataset\smart\tests"))
+def main(
+    st_companies_path: str | Path,
+    matching_sample_path: str | Path,
+):
+    QuantativeResultsMaker.make_and_save_quantative_results_df(
+        st_companies_path=st_companies_path,
+        matching_sample_path=matching_sample_path,
+        target_dir=r"",
+        result_path=r"",
+    )
 
 
 if __name__ == '__main__':
     st_companies_path_ = r"D:\dataset\smart\original_data\csmar\st_companies_in_2024.xlsx"
     matching_sample_path_ = r"D:\dataset\smart\original_data\csmar\matching_sample_1_in_2024.xlsx"
-    main()
+    main(
+        st_companies_path=st_companies_path_,
+        matching_sample_path=matching_sample_path_,
+    )
 
