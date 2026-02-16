6a1446fcd495af2e1d29c26f51f9f47b8b4d498c
yu <yul1024@outlook.com>
2026-02-16 20:14:00 +0800

    feat: add scripts to make qwen-long cache

---
+++ scripts/make_cache/make_qwen_long_cache.py
@@ -0,0 +1,22 @@
+"""
+构建基于 qwen-long 的 cache 。
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from data_processing.cache.dashscope_reader import (
+    QwenLongReader,
+)
+from data_processing.cache.openai_file_id_and_name_mapping import (
+    OpenAIFileIdAndNameMappingMethods,
+)
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+
+if __name__ == '__main__':
+    ...
+
