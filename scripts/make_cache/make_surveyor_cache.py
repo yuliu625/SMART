ed6fc9d5684bde1dae467088a37d489131ebca53
yu <yul1024@outlook.com>
2026-02-16 20:14:10 +0800

    feat: add scripts to make surveyor cache

---
+++ scripts/make_cache/make_surveyor_cache.py
@@ -0,0 +1,20 @@
+"""
+构建 MAS 运行可直接使用的 cache 。
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from data_processing.cache.openai_completion_parser import (
+    OpenAICompletionParser,
+)
+
+from pathlib import Path
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+if __name__ == '__main__':
+    ...
+
