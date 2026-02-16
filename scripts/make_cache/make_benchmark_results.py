d654682350d3b7bc9ed389796e04de99f80f0f29
yu <yul1024@outlook.com>
2026-02-16 20:14:25 +0800

    feat: add scripts to make benchmark results

---
+++ scripts/make_cache/make_benchmark_results.py
@@ -0,0 +1,20 @@
+"""
+独立构建 benchmark 的结果。
+"""
+
+from __future__ import annotations
+from loguru import logger
+
+from mas.cached_io_methods import (
+    CachedIOMethods,
+)
+
+from pathlib import Path
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+if __name__ == '__main__':
+    ...
+
