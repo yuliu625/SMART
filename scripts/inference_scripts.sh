8e9ec7884cb2cfc22334a5077ebaa4ac5d9b4184
yu <yul1024@outlook.com>
2026-02-16 19:56:13 +0800

    feat: add scripts to run inference server

---
+++ scripts/inference_scripts.sh
@@ -0,0 +1,13 @@
+# litellm proxy
+## normal sever
+litellm --config "D:\document\code\paper\SMART\configs\litellm\normal_configs.yaml"
+## test
+litellm --config "D:\document\code\paper\SMART\configs\litellm\local_test_configs.yaml"
+
+
+# vllm
+
+
+# sglang
+
+
