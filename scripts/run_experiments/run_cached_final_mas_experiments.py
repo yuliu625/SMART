fc6e34570b1af8ef34eb4b582ffb11b0a8718968
yu <yul1024@outlook.com>
2026-02-17 20:17:10 +0800

    to: add scripts to run experiments

---
+++ scripts/run_experiments/run_cached_final_mas_experiments.py
@@ -0,0 +1,21 @@
+"""
+Design Pattern: MAS.
+"""
+
+from __future__ import annotations
+import asyncio
+from loguru import logger
+
+from mas_runner.cached_final_mas_runner import (
+    CachedFinalMASRunner,
+)
+
+from pathlib import Path
+
+from typing import TYPE_CHECKING
+# if TYPE_CHECKING:
+
+
+if __name__ == '__main__':
+    ...
+
