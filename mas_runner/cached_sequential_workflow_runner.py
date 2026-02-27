3ffa69594c1e5014ae2af50c4c32dfbf985f582a
yu <yul1024@outlook.com>
2026-02-27 20:09:50 +0800

    feat: add cached runner

--- mas_runner/cached_sequential_workflow_runner.py
+++ mas_runner/cached_sequential_workflow_runner.py
@@ -8,7 +8,7 @@ from __future__ import annotations
 from loguru import logger
 
 from mas.cached_mas_factory import CachedMASFactory
-from mas.qdrant_rag_factory import QdrantRagFactory
+from mas.qdrant_rag_factory import QdrantRAGFactory
 from mas.cached_io_methods import CachedIOMethods
 
 from pathlib import Path
@@ -19,10 +19,129 @@ from typing import TYPE_CHECKING
 
 class CachedSequentialWorkflowRunner:
     @staticmethod
-    async def run_sequential_workflow_with_simple_rag():
-        ...
+    async def run_cached_sequential_workflow_with_simple_rag(
+        # IO
+        surveyor_cache_dir: str | Path,
+        general_query_path: str | Path,
+        vector_store_dir: str | Path,
+        result_dir: str | Path,
+        # MAS configurations
+        llm_base_url: str,
+        surveyor_main_llm_system_message_template_path: str,
+        adjudicator_main_llm_system_message_template_path: str,
+        adjudicator_formatter_llm_system_message_template_path: str,
+        # RAG configurations
+        collection_name: str,
+        embedding_model_name_or_path: str,
+        batch_size: int,
+        # Retriever
+        search_configs: dict,
+    ):
+        # 路径处理。
+        result_dir = Path(result_dir)
+        result_dir.mkdir(parents=True, exist_ok=True)
+        surveyor_cache_dir = Path(surveyor_cache_dir)
+        surveyor_cache_path_list = list(surveyor_cache_dir.glob('*.txt'))
+        vector_store_dir = Path(vector_store_dir)
+        for surveyor_cache_path in surveyor_cache_path_list:
+            result_file_path = result_dir / f"{surveyor_cache_path.stem}.json"
+            vector_store_path = vector_store_dir / f"{surveyor_cache_path.stem}"
+            if not vector_store_path.exists():
+                logger.error(f"Vector store {vector_store_path} does not exist")
+                continue
+            if not result_file_path.exists():
+                # build rag
+                rag = QdrantRAGFactory.create_simple_rag(
+                    client_disk_path=vector_store_path,
+                    collection_name=collection_name,
+                    embedding_model_name_or_path=embedding_model_name_or_path,
+                    batch_size=batch_size,
+                    search_configs=search_configs,
+                )
+                # build mas
+                mas = CachedMASFactory.create_cached_sequential_workflow(
+                    llm_base_url=llm_base_url,
+                    surveyor_main_llm_system_message_template_path=surveyor_main_llm_system_message_template_path,
+                    adjudicator_main_llm_system_message_template_path=adjudicator_main_llm_system_message_template_path,
+                    adjudicator_formatter_llm_system_message_template_path=adjudicator_formatter_llm_system_message_template_path,
+                    rag=rag,
+                )
+                state = CachedIOMethods.load_cached_sequential_workflow_state(
+                    surveyor_cache_path=surveyor_cache_path,
+                    general_query_path=general_query_path,
+                )
+                result = await mas.ainvoke(
+                    input=state,
+                )
+                CachedIOMethods.save_cached_sequential_workflow_state(
+                    state=result,
+                    result_path=result_file_path,
+                )
+                logger.success(f"Saved {result_file_path}.")
 
     @staticmethod
-    async def run_sequential_workflow_with_multi_query_rag():
-        ...
+    async def run_sequential_workflow_with_multi_query_rag(
+        # IO
+        surveyor_cache_dir: str | Path,
+        general_query_path: str | Path,
+        vector_store_dir: str | Path,
+        result_dir: str | Path,
+        # MAS configurations
+        llm_base_url: str,
+        surveyor_main_llm_system_message_template_path: str,
+        adjudicator_main_llm_system_message_template_path: str,
+        adjudicator_formatter_llm_system_message_template_path: str,
+        # RAG configurations
+        collection_name: str,
+        embedding_model_name_or_path: str,
+        batch_size: int,
+        # Retriever
+        search_configs: dict,
+        rewriter_llm_model_name: str,
+        rewriter_llm_system_message_template_path: str,
+    ):
+        # 路径处理。
+        result_dir = Path(result_dir)
+        result_dir.mkdir(parents=True, exist_ok=True)
+        surveyor_cache_dir = Path(surveyor_cache_dir)
+        surveyor_cache_path_list = list(surveyor_cache_dir.glob('*.txt'))
+        vector_store_dir = Path(vector_store_dir)
+        for surveyor_cache_path in surveyor_cache_path_list:
+            result_file_path = result_dir / f"{surveyor_cache_path.stem}.json"
+            vector_store_path = vector_store_dir / f"{surveyor_cache_path.stem}"
+            if not vector_store_path.exists():
+                logger.error(f"Vector store {vector_store_path} does not exist")
+                continue
+            if not result_file_path.exists():
+                # build rag
+                rag = QdrantRAGFactory.create_multi_query_rag(
+                    client_disk_path=vector_store_path,
+                    collection_name=collection_name,
+                    embedding_model_name_or_path=embedding_model_name_or_path,
+                    batch_size=batch_size,
+                    search_configs=search_configs,
+                    llm_base_url=llm_base_url,
+                    rewriter_llm_model_name=rewriter_llm_model_name,
+                    rewriter_llm_system_message_template_path=rewriter_llm_system_message_template_path,
+                )
+                # build mas
+                mas = CachedMASFactory.create_cached_sequential_workflow(
+                    llm_base_url=llm_base_url,
+                    surveyor_main_llm_system_message_template_path=surveyor_main_llm_system_message_template_path,
+                    adjudicator_main_llm_system_message_template_path=adjudicator_main_llm_system_message_template_path,
+                    adjudicator_formatter_llm_system_message_template_path=adjudicator_formatter_llm_system_message_template_path,
+                    rag=rag,
+                )
+                state = CachedIOMethods.load_cached_sequential_workflow_state(
+                    surveyor_cache_path=surveyor_cache_path,
+                    general_query_path=general_query_path,
+                )
+                result = await mas.ainvoke(
+                    input=state,
+                )
+                CachedIOMethods.save_cached_sequential_workflow_state(
+                    state=result,
+                    result_path=result_file_path,
+                )
+                logger.success(f"Saved {result_file_path}.")
 
