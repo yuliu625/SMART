347047280eebdddb8cb2c7c1143d15c286e46835
yu <yul1024@outlook.com>
2026-02-28 03:47:55 +0800

    feat: add scripts to run experiments

--- scripts/run_experiments/run_cached_sequential_workflow_experiments.py
+++ scripts/run_experiments/run_cached_sequential_workflow_experiments.py
@@ -16,6 +16,15 @@ from typing import TYPE_CHECKING
 # if TYPE_CHECKING:
 
 
+def get_common_io_configs() -> dict:
+    io_configs = dict(
+        surveyor_cache_dir=r"",
+        general_query_path=r"",
+        vector_store_dir=r"",
+    )
+    return io_configs
+
+
 def get_common_sequential_workflow_configs() -> dict:
     sequential_workflow_config = dict(
         llm_base_url='http://127.0.0.1:4000',
@@ -40,7 +49,7 @@ def get_common_multi_query_rag_configs() -> dict:
         collection_name='default',
         embedding_model_name_or_path=r"D:\model\BAAI\bge-m3",
         batch_size=1,
-        llm_base_url='http://127.0.0.1:4000',
+        # llm_base_url='http://127.0.0.1:4000',
         rewriter_llm_model_name='formatter-llm',
         rewriter_llm_system_message_template_path=r"D:\document\code\paper\SMART\mas\prompts\rag\rewriter_system_prompt_template.j2",
     )
@@ -59,28 +68,128 @@ def get_common_search_configs() -> dict:
     return search_configs
 
 
-async def run_dense_search_experiments():
-    ...
+async def run_dense_search_experiments(
+    max_concurrent: int,
+    result_dir: str,
+):
+    search_configs = get_common_search_configs()
+    search_configs['search_method'] = 'dense'
+    # batch run
+    ## simple rag
+    await CachedSequentialWorkflowRunner.batch_run_cached_sequential_workflow_with_simple_rag(
+        max_concurrent=max_concurrent,
+        result_dir=result_dir,
+        **get_common_io_configs(),
+        **get_common_sequential_workflow_configs(),
+        **get_common_simple_rag_configs(),
+        search_configs=search_configs,
+    )
+    logger.success(f"Dense Search Simple RAG Complete")
 
 
-async def run_sparse_search_experiments():
-    ...
+async def run_sparse_search_experiments(
+    max_concurrent: int,
+    result_dir: str,
+):
+    search_configs = get_common_search_configs()
+    search_configs['search_method'] = 'sparse'
+    # batch run
+    ## simple rag
+    await CachedSequentialWorkflowRunner.batch_run_cached_sequential_workflow_with_simple_rag(
+        max_concurrent=max_concurrent,
+        result_dir=result_dir,
+        **get_common_io_configs(),
+        **get_common_sequential_workflow_configs(),
+        **get_common_simple_rag_configs(),
+        **search_configs,
+        search_configs=search_configs,
+    )
+    logger.success(f"Sparse Search Simple RAG Complete")
 
 
-async def run_multi_vector_search_experiments():
-    ...
+async def run_multi_vector_search_experiments(
+    max_concurrent: int,
+    result_dir: str,
+):
+    search_configs = get_common_search_configs()
+    search_configs['search_method'] = 'multi_vector'
+    # batch run
+    ## simple rag
+    await CachedSequentialWorkflowRunner.batch_run_cached_sequential_workflow_with_simple_rag(
+        max_concurrent=max_concurrent,
+        result_dir=result_dir,
+        **get_common_io_configs(),
+        **get_common_sequential_workflow_configs(),
+        **get_common_simple_rag_configs(),
+        search_configs=search_configs,
+    )
+    logger.success(f"Multi-Vector Search Simple RAG Complete")
 
 
-async def run_hybrid_search_experiments():
-    ...
+async def run_hybrid_search_experiments(
+    max_concurrent: int,
+    result_dir: str,
+):
+    search_configs = get_common_search_configs()
+    search_configs['search_method'] = 'hybrid'
+    # batch run
+    ## simple rag
+    await CachedSequentialWorkflowRunner.batch_run_cached_sequential_workflow_with_simple_rag(
+        max_concurrent=max_concurrent,
+        result_dir=result_dir,
+        **get_common_io_configs(),
+        **get_common_sequential_workflow_configs(),
+        **get_common_simple_rag_configs(),
+        search_configs=search_configs,
+    )
+    logger.success(f"Hybrid Search Simple RAG Complete")
 
 
-async def run_all_search_experiments():
-    ...
+async def run_all_search_experiments(
+    max_concurrent: int,
+    result_dir: str,
+):
+    search_configs = get_common_search_configs()
+    search_configs['search_method'] = 'all'
+    # batch run
+    ## simple rag
+    await CachedSequentialWorkflowRunner.batch_run_cached_sequential_workflow_with_simple_rag(
+        max_concurrent=max_concurrent,
+        result_dir=result_dir,
+        **get_common_io_configs(),
+        **get_common_sequential_workflow_configs(),
+        **get_common_simple_rag_configs(),
+        search_configs=search_configs,
+    )
+    logger.success(f"All Search Simple RAG Complete")
 
 
 async def main():
-    ...
+    # dense
+    await run_dense_search_experiments(
+        max_concurrent=10,
+        result_dir=r"",
+    )
+    # sparse
+    await run_sparse_search_experiments(
+        max_concurrent=10,
+        result_dir=r"",
+    )
+    # multi-vector
+    await run_multi_vector_search_experiments(
+        max_concurrent=10,
+        result_dir=r"",
+    )
+    # hybrid
+    await run_hybrid_search_experiments(
+        max_concurrent=10,
+        result_dir=r"",
+    )
+    # all
+    await run_all_search_experiments(
+        max_concurrent=10,
+        result_dir=r"",
+    )
 
 
 if __name__ == '__main__':
