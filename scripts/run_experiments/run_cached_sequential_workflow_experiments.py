cb405378e9c192cf6140e786728bd4355e4defed
yu <yul1024@outlook.com>
2026-02-28 17:21:24 +0800

    feat: add scripts to run experiments

--- scripts/run_experiments/run_cached_sequential_workflow_experiments.py
+++ scripts/run_experiments/run_cached_sequential_workflow_experiments.py
@@ -1,5 +1,7 @@
 """
 Design Pattern: sequential workflow.
+
+对于 sequential workflow ，默认只进行 simple RAG 下各种 retrieval methods 的对比。
 """
 
 from __future__ import annotations
@@ -18,9 +20,9 @@ from typing import TYPE_CHECKING
 
 def get_common_io_configs() -> dict:
     io_configs = dict(
-        surveyor_cache_dir=r"",
-        general_query_path=r"",
-        vector_store_dir=r"",
+        surveyor_cache_dir=r"/home/liuyu/liuyu_nfs_data/smart_cache/surveyor_cache",
+        general_query_path=r"/home/liuyu/workspace/code/smart_/mas/prompts/rag/general_query_human_prompt_template.j2",
+        vector_store_dir=r"/home/liuyu/liuyu_nfs_data/smart_bge_m3_vector_store",
     )
     return io_configs
 
@@ -28,9 +30,9 @@ def get_common_io_configs() -> dict:
 def get_common_sequential_workflow_configs() -> dict:
     sequential_workflow_config = dict(
         llm_base_url='http://127.0.0.1:4000',
-        surveyor_main_llm_system_message_template_path=r"D:\document\code\paper\SMART\mas\prompts\sequential_workflow\surveyor_main_llm_system_prompt_template.j2",
-        adjudicator_main_llm_system_message_template_path=r"D:\document\code\paper\SMART\mas\prompts\sequential_workflow\adjudicator_main_llm_system_prompt_template.j2",
-        adjudicator_formatter_llm_system_message_template_path=r"D:\document\code\paper\SMART\mas\prompts\sequential_workflow\adjudicator_formatter_llm_system_prompt_template.j2",
+        surveyor_main_llm_system_message_template_path=r"/home/liuyu/workspace/code/smart_/mas/prompts/sequential_workflow/surveyor_main_llm_system_prompt_template.j2",
+        adjudicator_main_llm_system_message_template_path=r"/home/liuyu/workspace/code/smart_/mas/prompts/sequential_workflow/adjudicator_main_llm_system_prompt_template.j2",
+        adjudicator_formatter_llm_system_message_template_path=r"/home/liuyu/workspace/code/smart_/mas/prompts/sequential_workflow/adjudicator_formatter_llm_system_prompt_template.j2",
     )
     return sequential_workflow_config
 
@@ -48,10 +50,10 @@ def get_common_multi_query_rag_configs() -> dict:
     multi_query_rag_configs = dict(
         collection_name='default',
         embedding_model_name_or_path=r"/home/liuyu/liuyu_nfs_data/model/BAAI/bge-m3",
-        batch_size=1,
+        batch_size=8,
         # llm_base_url='http://127.0.0.1:4000',
         rewriter_llm_model_name='formatter-llm',
-        rewriter_llm_system_message_template_path=r"D:\document\code\paper\SMART\mas\prompts\rag\rewriter_system_prompt_template.j2",
+        rewriter_llm_system_message_template_path=r"/home/liuyu/workspace/code/smart_/mas/prompts/rag/rewriter_system_prompt_template.j2",
     )
     return multi_query_rag_configs
 
@@ -168,27 +170,27 @@ async def main():
     # dense
     await run_dense_search_experiments(
         max_concurrent=10,
-        result_dir=r"",
+        result_dir=r"/home/liuyu/liuyu_nfs_data/new_smart_sequential_workflow_simple_rag/dense",
     )
     # sparse
     await run_sparse_search_experiments(
         max_concurrent=10,
-        result_dir=r"",
+        result_dir=r"/home/liuyu/liuyu_nfs_data/new_smart_sequential_workflow_simple_rag/sparse",
     )
     # multi-vector
     await run_multi_vector_search_experiments(
         max_concurrent=10,
-        result_dir=r"",
+        result_dir=r"/home/liuyu/liuyu_nfs_data/new_smart_sequential_workflow_simple_rag/multi_vector",
     )
     # hybrid
     await run_hybrid_search_experiments(
         max_concurrent=10,
-        result_dir=r"",
+        result_dir=r"/home/liuyu/liuyu_nfs_data/new_smart_sequential_workflow_simple_rag/hybrid",
     )
     # all
     await run_all_search_experiments(
         max_concurrent=10,
-        result_dir=r"",
+        result_dir=r"/home/liuyu/liuyu_nfs_data/new_smart_sequential_workflow_simple_rag/all",
     )
 
 
