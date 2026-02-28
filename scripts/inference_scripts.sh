1b8a576587d25903e89c3685240b3c27284a608f
yu <yul1024@outlook.com>
2026-02-28 17:21:30 +0800

    feat: add scripts to run experiments

--- scripts/inference_scripts.sh
+++ scripts/inference_scripts.sh
@@ -1,5 +1,8 @@
 # litellm proxy
+## if set api key
+export DASHSCOPE_API_KEY=
 ## normal sever
+litellm --config "/home/liuyu/workspace/code/smart_/configs/litellm/normal_configs.yaml"
 litellm --config "D:\document\code\paper\SMART\configs\litellm\normal_configs.yaml"
 ## test
 litellm --config "D:\document\code\paper\SMART\configs\litellm\local_test_configs.yaml"
