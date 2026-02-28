0da0d0a58ec5d760208fe12a58d4db3a6fe07c82
yu <yul1024@outlook.com>
2026-02-28 19:19:03 +0800

    feat: add scripts to run experiments

--- scripts/tmux_scripts.sh
+++ scripts/tmux_scripts.sh
@@ -1,3 +1,28 @@
 # to project path
-cd /home/liuyu/workspace/code_/smart
+cd /home/liuyu/workspace/code/smart_
+
+# gateway
+## new task
+tmux new -s litellm_proxy
+## attach
+tmux a -t litellm_proxy
+conda activate smart_env
+export DASHSCOPE_API_KEY=
+litellm --config "/home/liuyu/workspace/code/smart_/configs/litellm/normal_configs.yaml"
+
+
+# experiments
+## sequential workflow
+#tmux new -s sw
+tmux a -t sw
+conda activate smart_env
+cd /home/liuyu/workspace/code/smart_
+python run_cached_sequential_workflow_experiments.py
+
+## mas
+#tmux new -s mas
+tmux a -t mas
+conda activate smart_env
+cd /home/liuyu/workspace/code/smart_
+python run_cached_final_mas_experiments.py
 
