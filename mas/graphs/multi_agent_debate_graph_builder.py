d350b18933f4b2dfd8c724a5c3998bae84e91b5c
yu <yul1024@outlook.com>
2026-04-02 12:10:57 +0800

    feat: add multi-agent debate graph builder

--- mas/graphs/multi_agent_debate_graph_builder.py
+++ mas/graphs/multi_agent_debate_graph_builder.py
@@ -1,10 +1,18 @@
 """
-这是一个graph-builder的Boilerplate。复制下面的代码，然后在具体项目中进行修改。
+Design Pattern: multi-agent debate
 """
 
 from __future__ import annotations
+from loguru import logger
 
-# from ? import MASState  # 由于构建graph_builder需要使用到MASState，因此不能仅以类型声明。
+# State
+from mas.schemas.multi_agent_debate_state import MultiAgentDebateState
+# Nodes
+from mas.agent_nodes.analysis_agents.proponent import Proponent
+from mas.agent_nodes.analysis_agents.opponent import Opponent
+from mas.agent_nodes.decision_agents.surveyor import Surveyor
+from mas.agent_nodes.decision_agents.adjudicator import Adjudicator
+# Edges
 
 from langgraph.graph import (
     StateGraph,
@@ -24,7 +32,7 @@ class MultiAgentDebateGraphBuilder:
     """
     def __init__(
         self,
-        state#: type[MASState],
+        state: type[MultiAgentDebateState],
     ):
         # 初始化计算图构建。实际中不会以变量传入state，因为state为数据类，更多实现方法为以包导入并写死。
         self.graph_builder = StateGraph(state)
@@ -33,20 +41,61 @@ class MultiAgentDebateGraphBuilder:
 
     def build_graph(
         self,
+        surveyor: Surveyor,
+        adjudicator: Adjudicator,
+        proponent: Proponent,
+        opponent: Opponent,
+        rag,
         checkpointer: BaseCheckpointSaver | None = None,
     ) -> CompiledStateGraph:
-        self._add_nodes()
+        self._add_nodes(
+            surveyor=surveyor,
+            adjudicator=adjudicator,
+            proponent=proponent,
+            opponent=opponent,
+            rag=rag,
+        )
         self._add_edges()
         graph = self.graph_builder.compile(checkpointer=checkpointer)
         return graph
 
-    def _add_nodes(self):
+    def _add_nodes(
+        self,
+        surveyor: Surveyor,
+        adjudicator: Adjudicator,
+        proponent: Proponent,
+        opponent: Opponent,
+        rag,
+    ):
         """
-        注册MAS的nodes。
+        注册 MAS 的 nodes 。
         """
+        # Decision Module
+        self.graph_builder.add_node('surveyor', surveyor.process_state)
+        self.graph_builder.add_node('adjudicator', adjudicator.process_state)
+        # Analysis Module
+        self.graph_builder.add_node('proponent', proponent.process_state)
+        self.graph_builder.add_node('opponent', opponent.process_state)
+        # RAG
+        self.graph_builder.add_node('rag', rag.process_state)
+
 
     def _add_edges(self):
         """
-        注册MAS的edges。
+        注册 MAS 的 edges 。
         """
+        # 输入的内容由 surveyor 进行处理。
+        self.graph_builder.add_edge(START, 'surveyor')
+        # Proponent 和 opponent 交替进行分析和判断。
+        ## 首先约定由 proponent 开始。
+        self.graph_builder.add_edge('surveyor', 'proponent')
+        ## proponent 根据情况选择选择 rag opponent adjudicator 。
+
+        ## opponent 根据情况选择选择 rag proponent adjudicator 。
+
+        # RAG 返回的结果一定交给 proponent 或 opponent 进行处理。
+        self.graph_builder.add_edge('rag', 'proponent')
+        self.graph_builder.add_edge('rag', 'opponent')
+        # Adjudicator 读取全部的信息，并做出最终的判断。
+        self.graph_builder.add_edge('adjudicator', END)
 
