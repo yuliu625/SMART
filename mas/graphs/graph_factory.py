a691c77fbc6a1894d664b1b25fbb264c023bd709
yu <yul1024@outlook.com>
2026-04-08 18:56:52 +0800

    feat: add multi-agent debate graph

--- mas/graphs/graph_factory.py
+++ mas/graphs/graph_factory.py
@@ -11,19 +11,24 @@ from mas.agent_nodes.agent_factory import AgentFactory
 from mas.agent_nodes.decision_agents.surveyor import Surveyor
 from mas.agent_nodes.decision_agents.investigator import Investigator
 from mas.agent_nodes.decision_agents.adjudicator import Adjudicator
-## Analysis Agent
+## Analysis Agents
 from mas.agent_nodes.analysis_agents.analyst import Analyst
+from mas.agent_nodes.analysis_agents.proponent import Proponent
+from mas.agent_nodes.analysis_agents.opponent import Opponent
 # RAG
 # from mas.rag_nodes.chroma_rag_builder import ChromaRAGBuilder
+# from mas.rag_nodes.qdrant_rag_builder import QdrantRAGBuilder
 # MAS
-## single agent mas
+## Single Agent
 from mas.graphs.single_agent_graph_builder import SingleAgentGraphBuilder
 from mas.schemas.single_agent_state import SingleAgentState
-## sequential mas
+## Sequential Workflow
 from mas.graphs.sequential_workflow_graph_builder import SequentialWorkflowGraphBuilder
 from mas.schemas.sequential_workflow_state import SequentialWorkflowState
-## multi agent debate
-## final mas
+## Multi-Agent Debate
+from mas.graphs.multi_agent_debate_graph_builder import MultiAgentDebateGraphBuilder
+from mas.schemas.multi_agent_debate_state import MultiAgentDebateState
+## final MAS
 from mas.graphs.final_mas_graph_builder import FinalMASGraphBuilder
 from mas.schemas.final_mas_state import FinalMASState
 
@@ -100,10 +105,73 @@ class GraphFactory:
         return graph
 
     @staticmethod
-    def create_multi_agent_debate_mas_graph(
-
+    def create_multi_agent_debate_graph(
+        # Surveyor
+        surveyor_main_llm: BaseChatModel,
+        surveyor_main_llm_system_message: SystemMessage,
+        # Proponent
+        proponent_main_llm: BaseChatModel,
+        proponent_main_llm_system_message: SystemMessage,
+        proponent_formatter_llm: BaseChatModel,
+        proponent_formatter_llm_system_message: SystemMessage,
+        proponent_structured_output_format: type[BaseModel],
+        # Opponent
+        opponent_main_llm: BaseChatModel,
+        opponent_main_llm_system_message: SystemMessage,
+        opponent_formatter_llm: BaseChatModel,
+        opponent_formatter_llm_system_message: SystemMessage,
+        opponent_structured_output_format: type[BaseModel],
+        # Adjudicator
+        adjudicator_main_llm: BaseChatModel,
+        adjudicator_main_llm_system_message: SystemMessage,
+        adjudicator_formatter_llm: BaseChatModel,
+        adjudicator_formatter_llm_system_message: SystemMessage,
+        adjudicator_structured_output_format: type[BaseModel],
+        # RAG
+        rag,
     ) -> CompiledStateGraph:
-        raise NotImplementedError
+        graph_builder = MultiAgentDebateGraphBuilder(state=MultiAgentDebateState)
+        # Surveyor
+        surveyor = AgentFactory.create_surveyor(
+            main_llm=surveyor_main_llm,
+            main_llm_system_message=surveyor_main_llm_system_message,
+        )
+        assert isinstance(surveyor, Surveyor)
+        # Adjudicator
+        adjudicator = AgentFactory.create_adjudicator(
+            main_llm=adjudicator_main_llm,
+            main_llm_system_message=adjudicator_main_llm_system_message,
+            formatter_llm=adjudicator_formatter_llm,
+            schema_pydantic_base_model=adjudicator_structured_output_format,
+            formatter_llm_system_message=adjudicator_formatter_llm_system_message,
+        )
+        assert isinstance(adjudicator, Adjudicator)
+        # Proponent
+        proponent = AgentFactory.create_proponent(
+            main_llm=proponent_main_llm,
+            main_llm_system_message=proponent_main_llm_system_message,
+            schema_pydantic_base_model=proponent_structured_output_format,
+            formatter_llm=proponent_formatter_llm,
+            formatter_llm_system_message=proponent_formatter_llm_system_message,
+        )
+        assert isinstance(proponent, Proponent)
+        # Opponent
+        opponent = AgentFactory.create_opponent(
+            main_llm=opponent_main_llm,
+            main_llm_system_message=opponent_main_llm_system_message,
+            schema_pydantic_base_model=opponent_structured_output_format,
+            formatter_llm=opponent_formatter_llm,
+            formatter_llm_system_message=opponent_formatter_llm_system_message,
+        )
+        assert isinstance(opponent, Opponent)
+        graph = graph_builder.build_graph(
+            surveyor=surveyor,
+            adjudicator=adjudicator,
+            proponent=proponent,
+            opponent=opponent,
+            rag=rag,
+        )
+        return graph
 
     @staticmethod
     def create_final_mas_graph(
